9e1366da678b1d492b34879760896ff143165517
Laurent Morichetti <laurent.morichetti@amd.com>
2020-05-27 23:32:04 -0700

    Remove the gfx909 architecture
    bfd/ChangeLog:
            * bfd/archures.c: remove gfx909
            * bfd/elf64-amdgcn.c: same
    
    Change-Id: I5a331b8187fdd844e96de087d3144ede81814b60

--- bfd/archures.c
+++ bfd/archures.c
@@ -555,7 +555,6 @@ DESCRIPTION
 .#define bfd_mach_amdgcn_gfx904  0x02e
 .#define bfd_mach_amdgcn_gfx906  0x02f
 .#define bfd_mach_amdgcn_gfx908  0x030
-.#define bfd_mach_amdgcn_gfx909  0x031
 .#define bfd_mach_amdgcn_gfx1010 0x033
 .#define bfd_mach_amdgcn_gfx1011 0x034
 .#define bfd_mach_amdgcn_gfx1012 0x035
