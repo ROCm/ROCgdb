e867e573288db927e56c35dbfde3f4db81ee0420
Pedro Alves <pedro@palves.net>
2021-08-26 13:27:45 +0100

    Flush dcache if lane changes
    GDB's dcache is flushed whenever we switch threads.  However, this is
    not safe enough for all address spaces considering that some of them
    might be local to a lane.  This patch makes GDB flush the dcache if
    the lane changes as well.
    
    Change-Id: Iaeded6a0b04ee200ac8bab9b6c9ddea83b8248d1

--- gdb/dcache.c
+++ gdb/dcache.c
@@ -114,8 +114,11 @@ struct dcache_struct
   int size;
   CORE_ADDR line_size;  /* current line_size.  */
 
-  /* The ptid of last inferior to use cache or null_ptid.  */
+  /* The ptid of the last thread to use the cache or null_ptid.  */
   ptid_t ptid;
+
+  /* The selected lane of the last thread to use the cache or -1.  */
+  int lane;
 
   /* The process target of last inferior to use the cache or
      nullptr.  */
@@ -253,6 +256,7 @@ dcache_invalidate (DCACHE *dcache)
   dcache->oldest = NULL;
   dcache->size = 0;
   dcache->ptid = null_ptid;
+  dcache->lane = -1;
   dcache->proc_target = nullptr;
 
   if (dcache->line_size != dcache_line_size)
@@ -458,6 +462,7 @@ dcache_init (void)
   dcache->size = 0;
   dcache->line_size = dcache_line_size;
   dcache->ptid = null_ptid;
+  dcache->lane = -1;
   dcache->proc_target = nullptr;
 
   return dcache;
@@ -476,14 +481,24 @@ dcache_read_memory_partial (struct target_ops *ops, DCACHE *dcache,
 {
   ULONGEST i;
 
-  /* If this is a different thread from what we've recorded, flush the
-     cache.  */
+  /* If this is a different thread or lane from what we've recorded,
+     flush the cache.  */
+
+  /* When we're detaching breakpoints from a fork child before
+     detaching it ("set detach-on-fork on"), inferior_ptid points to
+     the child, and the current inferior points to the parent.  We can
+     detect the situation because the pids don't match.  */
+  int current_lane = (inferior_ptid.pid () != current_inferior ()->pid
+		      ? 0
+		      : inferior_thread ()->current_simd_lane ());
 
   process_stratum_target *proc_target = current_inferior ()->process_target ();
-  if (proc_target != dcache->proc_target || inferior_ptid != dcache->ptid)
+  if (proc_target != dcache->proc_target || inferior_ptid != dcache->ptid
+      || current_lane != dcache->lane)
     {
       dcache_invalidate (dcache);
       dcache->ptid = inferior_ptid;
+      dcache->lane = current_lane;
       dcache->proc_target = proc_target;
     }
 
