ROCm/ROCgdb

commit aba829e8d5e37ebb0c19eb9cdd1e91669d77c329

gdb: don't share aspace/pspace on fork
As part of SWDEV-300171, it was found that when handling forks, two
inferiors can unexpectedly share one program space and one address
space.  To reproduce:

 1. Using a test program that forks...
 2. "set follow-fork-mode child"
 3. "set detach-on-fork on" (the default)
 4. run to a breakpoint somewhere after the fork
 5. switch back to inferior 1
 6. run to somewhere before the fork (e.g use "start")

Step 4 should have created a new inferior:

    (gdb) info inferiors
      Num  Description       Connection           Executable
      1    <null>                                 /home/smarchi/build/wt/amd/gdb/fork
    * 2    process 251425    1 (native)           /home/smarchi/build/wt/amd/gdb/fork

After step 6, we have two unrelated live inferiors:

    (gdb) info inferiors
      Num  Description       Connection           Executable
    * 1    process 251432    1 (native)           /home/smarchi/build/wt/amd/gdb/fork
      2    process 251425    1 (native)           /home/smarchi/build/wt/amd/gdb/fork

By inspecting the state of GDB, we can see that the two inferiors now
share one program space and one address space:

    (top-gdb) p inferior_list.m_front.num
    $2 = 1
    (top-gdb) p inferior_list.m_front.aspace
    $3 = (struct address_space *) 0x5595e2520400
    (top-gdb) p inferior_list.m_front.pspace
    $4 = (struct program_space *) 0x5595e2520440
    (top-gdb) p inferior_list.m_front.next.num
    $5 = 2
    (top-gdb) p inferior_list.m_front.next.aspace
    $6 = (struct address_space *) 0x5595e2520400
    (top-gdb) p inferior_list.m_front.next.pspace
    $7 = (struct program_space *) 0x5595e2520440

In SWDEV-3000171, that translates to a

     Could not find breakpoint_id for breakpoint at 0x7ffff57ca3c0

message when an inferior, in a two-inferior scenario, hits a ROCm
internal breakpoint.  The inferior hits a breakpoint meant for the other
inferior, since they share the same program space.  But since

gdb/infrun.c
@@ -557,14 +557,19 @@ holding the child stopped.  Try \"set detach-on-fork\" or \
       program_space *parent_pspace = parent_inf->pspace;
 
       /* If this is a vfork child, then the address-space is shared
-	 with the parent.  If we detached from the parent, then we can
-	 reuse the parent's program/address spaces.  */
-      if (has_vforked || detach_fork)
+	 with the parent.  */
+      if (has_vforked)
 	{
 	  child_inf->pspace = parent_pspace;
 	  child_inf->aspace = child_inf->pspace->aspace;
 
 	  exec_on_vfork (child_inf);
+	}
+      else if (detach_fork)
+	{
+	  child_inf->aspace = new_address_space ();
+	  child_inf->pspace = new program_space (child_inf->aspace);
+	  clone_program_space (child_inf->pspace, parent_pspace);
 	}
       else
 	{

gdb/testsuite/gdb.rocm/multi-inferior-fork.cpp
@@ -1,0 +1,46 @@
+/* Copyright (C) 2021 Advanced Micro Devices, Inc. All rights reserved.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <unistd.h>
+#include <hip/hip_runtime.h>
+
+__global__ void
+kernel ()
+{}
+
+static void
+child_after_fork ()
+{}
+
+int
+main ()
+{
+  if (fork () != 0)
+    {
+      /* Parent.  */
+    }
+  else
+    {
+      /* Child.  */
+      child_after_fork ();
+      hipLaunchKernelGGL (kernel, dim3 (1), dim3 (1), 0, 0);
+      hipDeviceSynchronize ();
+    }
+
+  return 0;
+}

gdb/testsuite/gdb.rocm/multi-inferior-fork.exp
@@ -1,0 +1,78 @@
+# Copyright (C) 2021 Advanced Micro Devices, Inc. All rights reserved.
+
+# This file is part of GDB.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Verify that we can debug multiple programs that fork.  The bug that prompted
+# writing this test was that with "follow-fork-mode child", multiple inferiors
+# wrongly shared a single program space, leading to all sorts of problems.
+
+load_lib rocm.exp
+
+standard_testfile .cpp
+
+if {[build_executable "failed to prepare" ${binfile} ${srcfile} {debug hip}]} {
+    return
+}
+
+proc do_test { stop_at } {
+    clean_restart $::binfile
+
+    gdb_test_no_output "set follow-fork-mode child"
+
+    with_rocm_gpu_lock {
+	if { ![runto $stop_at allow-pending message] } {
+	    return
+	}
+
+	gdb_test "inferior" "Current inferior is 2 .*"
+	gdb_test "inferior 1" "Switching to inferior 1 .*"
+
+	gdb_test_multiple "run" "run" {
+	    -re "At least one agent is busy" {
+		if {[hcc_amdgpu_target] != "gfx90a"} {
+		    unsupported $gdb_test_name
+		} else {
+		    fail $gdb_test_name
+		}
+		return
+	    }
+
+	    -re -wrap "Thread 3.${::decimal} .* hit Breakpoint ${::decimal}, ${stop_at} .*" {
+		pass $gdb_test_name
+	    }
+	}
+
+	# Continue inferior 3 to end.  We must use extra=1 because there is a bit
+	# of expected chat between "Continuing." and the "Inferior exited normally"
+	# message.
+	gdb_continue_to_end "continue inferior 3 to end" "continue" 1
+
+	# Continue inferior 2 to end.
+	gdb_test "inferior 2"
+	gdb_continue_to_end "continue inferior 2 to end" "continue" 1
+    }
+}
+
+# Different modes of failure were seen when stopping in the kernel and in the
+# kernel, so test both.
+#
+# When stopped on `kernel` we would hit some internal error in rocm-tdep.c.
+#
+# When stopping on `child_after_fork`, trying to continue inferior 2 until the
+# end wouldn't work, the process was stuck on / kept hitting the breakpoint.
+foreach_with_prefix stop_at { kernel child_after_fork } {
+    do_test $stop_at
+}

commit 2416a3a2cfe93b4f301c2d533bb515b7e017deb1

gdb.base/foll-fork.exp: refactor to restart GDB between each portion of the test
Commit aba829e8d5e3 ("gdb: don't share aspace/pspace on fork") regressed
some fork-related tests, one of them being gdb.base/foll-fork.exp.  I
must have missed it, my bad.

All failures in that test are of the same kind, here's the first:

    (gdb) break callee^M
    Breakpoint 5 at 0x555555555194: callee. (2 locations)^M
    (gdb) FAIL: gdb.base/foll-fork.exp: unpatch child, break at callee

Compared to before:

    (gdb) break callee^M
    Breakpoint 5 at 0x555555555194: file /home/master/smarchi/src/binutils-gdb/gdb/testsuite/gdb.base/foll-fork.c, line 9.^M
    (gdb) PASS: gdb.base/foll-fork.exp: unpatch child, break at callee

Adding an "info breakpoints" just after this, we see what happens.
Before the patch:

    info breakpoints^M
    Num     Type           Disp Enb Address            What^M
    4       breakpoint     keep y   0x00005555555551a3 in main at /home/master/smarchi/src/binutils-gdb/gdb/testsuite/gdb.base/foll-fork.c:14 inf 1, 2^M
            breakpoint already hit 1 time^M
    5       breakpoint     keep y   0x0000555555555194 in callee at /home/master/smarchi/src/binutils-gdb/gdb/testsuite/gdb.base/foll-fork.c:9 inf 1, 2^M
    (gdb) PASS: gdb.base/foll-fork.exp: info breakpoints

After the patch:

    info breakpoints^M
    Num     Type           Disp Enb Address            What^M
    4       breakpoint     keep y   <MULTIPLE>         ^M
            breakpoint already hit 1 time^M
    4.1                         y   0x00005555555551a3 in main at /home/master/smarchi/src/binutils-gdb/gdb/testsuite/gdb.base/foll-fork.c:14 inf 1^M
    4.2                         y   0x00005555555551a3 in main at /home/master/smarchi/src/binutils-gdb/gdb/testsuite/gdb.base/foll-fork.c:14 inf 2^M
    5       breakpoint     keep y   <MULTIPLE>         ^M

gdb/testsuite/gdb.base/foll-fork.exp
@@ -22,12 +22,32 @@ if [gdb_debug_enabled] {
 
 standard_testfile
 
-if {[prepare_for_testing "failed to prepare" $testfile $srcfile debug]} {
+if {[build_executable "failed to prepare" $testfile $srcfile debug]} {
     return -1
 }
 
-proc check_fork_catchpoints {} {
+# Restart GDB and run the inferior to main.  Return 1 on success, 0 on failure.
+
+proc setup {} {
+    clean_restart $::testfile
+
+    if { ![runto_main] } {
+	fail "could not run to main"
+	return 0
+    }
+
+    return 1
+}
+
+# Check that fork catchpoints are supported, as an indicator for whether
+# fork-following is supported.  Return 1 if they are, else 0.
+
+proc_with_prefix check_fork_catchpoints {} {
   global gdb_prompt
+
+  if { ![setup] } {
+      return
+  }
 
   # Verify that the system supports "catch fork".
   gdb_test "catch fork" "Catchpoint \[0-9\]* \\(fork\\)" "insert first fork catchpoint"
@@ -42,10 +62,7 @@ proc check_fork_catchpoints {} {
     }
   }
 
-  if {$has_fork_catchpoints == 0} {
-    unsupported "fork catchpoints"
-    return -code return
-  }
+  return $has_fork_catchpoints
 }
 
 # Test follow-fork to ensure that the correct process is followed, that
@@ -57,7 +74,7 @@ proc check_fork_catchpoints {} {
 # execute the program past the fork.  If the value of WHO or DETACH is
 # 'default', the corresponding GDB command is skipped for that test.
 # The value of CMD must be either 'next 2' or 'continue'.
-proc test_follow_fork { who detach cmd } {
+proc_with_prefix test_follow_fork { who detach cmd } {
     global gdb_prompt
     global srcfile
     global testfile
@@ -65,11 +82,8 @@ proc test_follow_fork { who detach cmd } {
     with_test_prefix "follow $who, detach $detach, command \"$cmd\"" {
 
 	# Start a new debugger session each time so defaults are legitimate.
-	clean_restart $testfile
-
-	if ![runto_main] {
-	    untested "could not run to main"
-	    return -1
+	if { ![setup] } {
+	    return
 	}
 
 	# The "Detaching..." and "Attaching..." messages may be hidden by
@@ -174,10 +188,17 @@ proc test_follow_fork { who detach cmd } {
 
 set reading_in_symbols_re {(?:\r\nReading in symbols for [^\r\n]*)?}
 
-proc catch_fork_child_follow {} {
+# Test the ability to catch a fork, specify that the child be
+# followed, and continue.  Make the catchpoint permanent.
+
+proc_with_prefix catch_fork_child_follow {} {
     global gdb_prompt
     global srcfile
     global reading_in_symbols_re
+
+    if { ![setup] } {
+	return
+    }
 
     set bp_after_fork [gdb_get_line_number "set breakpoint here"]
 
@@ -224,9 +245,17 @@ proc catch_fork_child_follow {} {
 	"y"
 }
 
-proc catch_fork_unpatch_child {} {
+# Test that parent breakpoints are successfully detached from the
+# child at fork time, even if the user removes them from the
+# breakpoints list after stopping at a fork catchpoint.
+
+proc_with_prefix catch_fork_unpatch_child {} {
     global gdb_prompt
     global srcfile
+
+    if { ![setup] } {
+	return
+    }
 
     set bp_exit [gdb_get_line_number "at exit"]
 
@@ -271,10 +300,17 @@ proc catch_fork_unpatch_child {} {
     }
 }
 
-proc tcatch_fork_parent_follow {} {
+# Test the ability to catch a fork, specify via a -do clause that
+# the parent be followed, and continue.  Make the catchpoint temporary.
+
+proc_with_prefix tcatch_fork_parent_follow {} {
     global gdb_prompt
     global srcfile
     global reading_in_symbols_re
+
+    if { ![setup] } {
+	return
+    }
 
     set bp_after_fork [gdb_get_line_number "set breakpoint here"]
 
@@ -313,9 +349,10 @@ proc tcatch_fork_parent_follow {} {
 	"y"
 }
 
-proc do_fork_tests {} {
-    global gdb_prompt
-    global testfile
+# Test simple things about the "set follow-fork-mode" command.
+
+proc_with_prefix test_set_follow_fork_command {} {
+    clean_restart
 
     # Verify that help is available for "set follow-fork-mode".
     #
@@ -342,56 +379,37 @@ By default, the debugger will follow the parent process..*"
 	"set follow-fork to nonsense is prohibited"
 
     gdb_test_no_output "set follow-fork parent" "reset parent"
-
-    # Check that fork catchpoints are supported, as an indicator for whether
-    # fork-following is supported.
-    if [runto_main] then { check_fork_catchpoints }
-
-    # Test the basic follow-fork functionality using all combinations of
-    # values for follow-fork-mode and detach-on-fork, using either a
-    # breakpoint or single-step to execute past the fork.
-    #
-    # The first loop should be sufficient to test the defaults.  There
-    # is no need to test using the defaults in other permutations (e.g.
-    # "default" "on", "parent" "default", etc.).
-    foreach cmd {"next 2" "continue"} {
-        test_follow_fork "default" "default" $cmd
-    }
-
-    # Now test all explicit permutations.
-    foreach who {"parent" "child"} {
-	foreach detach {"on" "off"} {
-	    foreach cmd {"next 2" "continue"} {
-		test_follow_fork $who $detach $cmd
-	    }
-	}
-    }
-
-    # Catchpoint tests.
-
-    # Restart to eliminate any effects of the follow-fork tests.
-    clean_restart $testfile
-    gdb_test_no_output "set verbose"
-
-    # Test the ability to catch a fork, specify that the child be
-    # followed, and continue.  Make the catchpoint permanent.
-    #
-    if [runto_main] then { catch_fork_child_follow }
-
-    # Test that parent breakpoints are successfully detached from the
-    # child at fork time, even if the user removes them from the
-    # breakpoints list after stopping at a fork catchpoint.
-    if [runto_main] then { catch_fork_unpatch_child }
-
-    # Test the ability to catch a fork, specify via a -do clause that
-    # the parent be followed, and continue.  Make the catchpoint temporary.
-    #
-    if [runto_main] then { tcatch_fork_parent_follow }
 }
 
-# This is a test of gdb's ability to follow the parent, child or both
-# parent and child of a Unix fork() system call.
-#
-do_fork_tests
+test_set_follow_fork_command
 
-return 0
+if { ![check_fork_catchpoints] } {
+    untested "follow-fork not supported"
+    return
+}
+
+# Test the basic follow-fork functionality using all combinations of
+# values for follow-fork-mode and detach-on-fork, using either a
+# breakpoint or single-step to execute past the fork.
+#
+# The first loop should be sufficient to test the defaults.  There
+# is no need to test using the defaults in other permutations (e.g.
+# "default" "on", "parent" "default", etc.).
+foreach cmd {"next 2" "continue"} {
+    test_follow_fork "default" "default" $cmd
+}
+
+# Now test all explicit permutations.
+foreach who {"parent" "child"} {
+    foreach detach {"on" "off"} {
+	foreach cmd {"next 2" "continue"} {
+	    test_follow_fork $who $detach $cmd
+	}
+    }
+}
+
+# Catchpoint tests.
+
+catch_fork_child_follow
+catch_fork_unpatch_child
+tcatch_fork_parent_follow