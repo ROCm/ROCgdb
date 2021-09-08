ROCm/ROCgdb

commit aba829e8d5e37ebb0c19eb9cdd1e91669d77c329

gdb: don't share aspace/pspace on fork
As part of SWDEV-300171, it was found that when handling forks, two
inferiors can unexpectedly share one program space and one address
space.  To reproduce:

 1. Using a test program that forks...
 2. "set follow-fork-mode child"
 3. "set detach-on-fork on" (the default)
 4. run to a breakpoint somewhere after the fork
 5. switch back to inferior 1
 6. run to somewhere before the fork (e.g use "start")

Step 4 should have created a new inferior:

    (gdb) info inferiors
      Num  Description       Connection           Executable
      1    <null>                                 /home/smarchi/build/wt/amd/gdb/fork
    * 2    process 251425    1 (native)           /home/smarchi/build/wt/amd/gdb/fork

After step 6, we have two unrelated live inferiors:

    (gdb) info inferiors
      Num  Description       Connection           Executable
    * 1    process 251432    1 (native)           /home/smarchi/build/wt/amd/gdb/fork
      2    process 251425    1 (native)           /home/smarchi/build/wt/amd/gdb/fork

By inspecting the state of GDB, we can see that the two inferiors now
share one program space and one address space:

    (top-gdb) p inferior_list.m_front.num
    $2 = 1
    (top-gdb) p inferior_list.m_front.aspace
    $3 = (struct address_space *) 0x5595e2520400
    (top-gdb) p inferior_list.m_front.pspace
    $4 = (struct program_space *) 0x5595e2520440
    (top-gdb) p inferior_list.m_front.next.num
    $5 = 2
    (top-gdb) p inferior_list.m_front.next.aspace
    $6 = (struct address_space *) 0x5595e2520400
    (top-gdb) p inferior_list.m_front.next.pspace
    $7 = (struct program_space *) 0x5595e2520440

In SWDEV-3000171, that translates to a

     Could not find breakpoint_id for breakpoint at 0x7ffff57ca3c0

message when an inferior, in a two-inferior scenario, hits a ROCm
internal breakpoint.  The inferior hits a breakpoint meant for the other
inferior, since they share the same program space.  But since

gdb/infrun.c
@@ -557,14 +557,19 @@ holding the child stopped.  Try \"set detach-on-fork\" or \
       program_space *parent_pspace = parent_inf->pspace;
 
       /* If this is a vfork child, then the address-space is shared
-	 with the parent.  If we detached from the parent, then we can
-	 reuse the parent's program/address spaces.  */
-      if (has_vforked || detach_fork)
+	 with the parent.  */
+      if (has_vforked)
 	{
 	  child_inf->pspace = parent_pspace;
 	  child_inf->aspace = child_inf->pspace->aspace;
 
 	  exec_on_vfork (child_inf);
+	}
+      else if (detach_fork)
+	{
+	  child_inf->aspace = new_address_space ();
+	  child_inf->pspace = new program_space (child_inf->aspace);
+	  clone_program_space (child_inf->pspace, parent_pspace);
 	}
       else
 	{

gdb/testsuite/gdb.rocm/multi-inferior-fork.cpp
@@ -1,0 +1,46 @@
+/* Copyright (C) 2021 Advanced Micro Devices, Inc. All rights reserved.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <unistd.h>
+#include <hip/hip_runtime.h>
+
+__global__ void
+kernel ()
+{}
+
+static void
+child_after_fork ()
+{}
+
+int
+main ()
+{
+  if (fork () != 0)
+    {
+      /* Parent.  */
+    }
+  else
+    {
+      /* Child.  */
+      child_after_fork ();
+      hipLaunchKernelGGL (kernel, dim3 (1), dim3 (1), 0, 0);
+      hipDeviceSynchronize ();
+    }
+
+  return 0;
+}

gdb/testsuite/gdb.rocm/multi-inferior-fork.exp
@@ -1,0 +1,78 @@
+# Copyright (C) 2021 Advanced Micro Devices, Inc. All rights reserved.
+
+# This file is part of GDB.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Verify that we can debug multiple programs that fork.  The bug that prompted
+# writing this test was that with "follow-fork-mode child", multiple inferiors
+# wrongly shared a single program space, leading to all sorts of problems.
+
+load_lib rocm.exp
+
+standard_testfile .cpp
+
+if {[build_executable "failed to prepare" ${binfile} ${srcfile} {debug hip}]} {
+    return
+}
+
+proc do_test { stop_at } {
+    clean_restart $::binfile
+
+    gdb_test_no_output "set follow-fork-mode child"
+
+    with_rocm_gpu_lock {
+	if { ![runto $stop_at allow-pending message] } {
+	    return
+	}
+
+	gdb_test "inferior" "Current inferior is 2 .*"
+	gdb_test "inferior 1" "Switching to inferior 1 .*"
+
+	gdb_test_multiple "run" "run" {
+	    -re "At least one agent is busy" {
+		if {[hcc_amdgpu_target] != "gfx90a"} {
+		    unsupported $gdb_test_name
+		} else {
+		    fail $gdb_test_name
+		}
+		return
+	    }
+
+	    -re -wrap "Thread 3.${::decimal} .* hit Breakpoint ${::decimal}, ${stop_at} .*" {
+		pass $gdb_test_name
+	    }
+	}
+
+	# Continue inferior 3 to end.  We must use extra=1 because there is a bit
+	# of expected chat between "Continuing." and the "Inferior exited normally"
+	# message.
+	gdb_continue_to_end "continue inferior 3 to end" "continue" 1
+
+	# Continue inferior 2 to end.
+	gdb_test "inferior 2"
+	gdb_continue_to_end "continue inferior 2 to end" "continue" 1
+    }
+}
+
+# Different modes of failure were seen when stopping in the kernel and in the
+# kernel, so test both.
+#
+# When stopped on `kernel` we would hit some internal error in rocm-tdep.c.
+#
+# When stopping on `child_after_fork`, trying to continue inferior 2 until the
+# end wouldn't work, the process was stuck on / kept hitting the breakpoint.
+foreach_with_prefix stop_at { kernel child_after_fork } {
+    do_test $stop_at
+}

commit c8c37dcdbb0928f5f165c3eac859e797a2226777

gdb: fix some "follow-fork-mode child"-related issues
Commit aba829e8d5e3 ("gdb: don't share aspace/pspace on fork") regressed
some fork-related tests:

gdb.threads/fork-child-threads.exp:

    (gdb) next
    [Attaching after Thread 0x7ffff7da7740 (LWP 2845728) fork to child process 2845736]
    [New inferior 2 (process 2845736)]
    [Detaching after fork from parent process 2845728]
    [Inferior 1 (process 2845728) detached]
    [Thread debugging using libthread_db enabled]
    Using host libthread_db library "/lib/x86_64-linux-gnu/libthread_db.so.1".
    [Switching to thread 2.1 (Thread 0x7ffff7da7740 (LWP 2845736))]
    main () at /home/master/smarchi/src/binutils-gdb/gdb/testsuite/gdb.threads/fork-child-threads.c:34
    34        switch (fork ())
    (gdb) FAIL: gdb.threads/fork-child-threads.exp: next over fork

The problem here is the "next" stopping to early when doing "next" over
fork(), following the child.  Since we create a new program space the
child, the child gets some brand new symtabs as well.  So the
thread_info::current_symtab field, set when starting the "next" to know
where we started from, points to a symtab of the parent.  When the child
thread stops just after the fork, GDB thinks the child has stopped in a
line in a different symtab it started from, and stops the next early.
In reality, the child has stopped on the same line the next started on,
just that it now has a different copy of the starting symtab.

gdb.threads/watchpoint-fork.exp:

    continue
    Continuing.
    [Attaching after process 2846229 fork to child process 2846237]
    [New inferior 2 (process 2846237)]
    [Detaching after fork from parent process 2846229]
    [Inferior 1 (process 2846229) detached]
    Warning:
    Could not insert hardware watchpoint 2.
    Could not insert hardware breakpoints:
    You may have requested too many hardware breakpoints/watchpoints.

    (gdb) FAIL: gdb.threads/watchpoint-fork.exp: child: singlethreaded: breakpoint after the first fork

gdb/infrun.c
@@ -554,22 +554,38 @@ holding the child stopped.  Try \"set detach-on-fork\" or \
       child_inf->gdbarch = parent_inf->gdbarch;
       copy_inferior_target_desc_info (child_inf, parent_inf);
 
-      program_space *parent_pspace = parent_inf->pspace;
-
-      /* If this is a vfork child, then the address-space is shared
-	 with the parent.  */
       if (has_vforked)
 	{
-	  child_inf->pspace = parent_pspace;
-	  child_inf->aspace = child_inf->pspace->aspace;
+	  /* If this is a vfork child, then the address-space is shared
+	     with the parent.  */
+	  child_inf->aspace = parent_inf->aspace;
+	  child_inf->pspace = parent_inf->pspace;
 
 	  exec_on_vfork (child_inf);
 	}
       else if (detach_fork)
 	{
-	  child_inf->aspace = new_address_space ();
-	  child_inf->pspace = new program_space (child_inf->aspace);
-	  clone_program_space (child_inf->pspace, parent_pspace);
+	  /* We follow the child and detach from the parent: move the parent's
+	     program space to the child.  This simplifies some things, like
+	     doing "next" over fork() and landing on the expected line in the
+	     child (note, that is broken with "set detach-on-fork off").
+
+	     Before assigning brand new spaces for the parent, remove
+	     breakpoints from it: because the new pspace won't match
+	     currently inserted locations, the normal detach procedure
+	     wouldn't remove them, and we would leave them inserted when
+	     detaching.  */
+	  remove_breakpoints_inf (parent_inf);
+
+	  child_inf->aspace = parent_inf->aspace;
+	  child_inf->pspace = parent_inf->pspace;
+	  parent_inf->aspace = new_address_space ();
+	  parent_inf->pspace = new program_space (parent_inf->aspace);
+	  clone_program_space (parent_inf->pspace, child_inf->pspace);
+
+	  /* The parent inferior is still the current one, so keep things
+	     in sync.  */
+	  set_current_program_space (parent_inf->pspace);
 	}
       else
 	{
@@ -577,7 +593,7 @@ holding the child stopped.  Try \"set detach-on-fork\" or \
 	  child_inf->pspace = new program_space (child_inf->aspace);
 	  child_inf->removable = 1;
 	  child_inf->symfile_flags = SYMFILE_NO_READ;
-	  clone_program_space (child_inf->pspace, parent_pspace);
+	  clone_program_space (child_inf->pspace, parent_inf->pspace);
 	}
     }
 