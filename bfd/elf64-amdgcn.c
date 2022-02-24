51e891fb4f07065e6f2b63d8076ab2e3226f222a
Simon Marchi <simon.marchi@efficios.com>
2022-02-24 16:20:43 -0500

    bfd/elf64-amdgcn: remove support for v2 code objects
    v2 code objects encode the specific processor version in an HSA-specific
    NT_AMD_HSA_ISA_VERSION note.
    
    v3 code objects and later use a simpler scheme where the specific
    processor version is encoded in the e_flags field (the sub-field
    EF_AMDGPU_MACH).
    
    Since it's no longer necessary to support v2 code objects, remove the
    code that reads the NT_AMD_HSA_ISA_VERSION note.  This helps simplify
    the code, prior to submitting this file upstream.
    
    Add the ELFOSABI values to include/elf/amdgcn.h, to replace the use of
    an hardcoded value.
    
    Change-Id: Ie601c6ef28d7e032f7d3c92474d14e81869f9c5e

--- bfd/elf64-amdgcn.c
+++ bfd/elf64-amdgcn.c
@@ -26,99 +26,26 @@
 
 #include <string.h>
 
-
-static unsigned int
-bfd_amdgcn_get_mach_from_notes (bfd *abfd)
-{
-  asection *     note_section;
-  bfd_size_type  buffer_size;
-  bfd_byte *     ptr, * end, * buffer = NULL;
-  unsigned int   mach = 0;
-
-  note_section = bfd_get_section_by_name (abfd, ".note");
-  if (note_section == NULL)
-    return 0;
-
-  buffer_size = note_section->size;
-  if (buffer_size == 0)
-    return 0;
-
-  if (!bfd_malloc_and_get_section (abfd, note_section, &buffer))
-    return 0;
-
-  ptr = buffer;
-  end = &buffer[buffer_size];
-  while (mach == 0 && buffer < end)
-    {
-      unsigned long namesz;
-      unsigned long descsz;
-      unsigned long type;
-
-      if ((ptr + 12)  >= end)
-        break;
-
-      namesz = bfd_get_32 (abfd, ptr);
-      descsz = bfd_get_32 (abfd, ptr + 4);
-      type   = bfd_get_32 (abfd, ptr + 8);
-
-      if (namesz == 4 && (ptr + 16) <= end
-          && (!strcmp((char*) ptr + 12, "AMDGPU")
-              || !strcmp((char*) ptr + 12, "AMD"))
-          && type == NT_AMDGPU_HSA_ISA)
-        {
-          unsigned int major, minor, patch;
-
-          major = bfd_get_32 (abfd, ptr + 20);
-          minor = bfd_get_32 (abfd, ptr + 24);
-          patch = bfd_get_32 (abfd, ptr + 28);
-
-#define GFX(major, minor, patch) (((major) << 16) + ((minor) << 8) + (patch))
-          switch (GFX (major, minor, patch))
-            {
-            case GFX (8, 0, 1): mach = bfd_mach_amdgcn_gfx801; break;
-            case GFX (8, 0, 2): mach = bfd_mach_amdgcn_gfx802; break;
-            case GFX (8, 0, 3): mach = bfd_mach_amdgcn_gfx803; break;
-            case GFX (8, 1, 0): mach = bfd_mach_amdgcn_gfx810; break;
-            case GFX (9, 0, 0): mach = bfd_mach_amdgcn_gfx900; break;
-            case GFX (9, 0, 2): mach = bfd_mach_amdgcn_gfx902; break;
-            case GFX (9, 0, 4): mach = bfd_mach_amdgcn_gfx904; break;
-            case GFX (9, 0, 6): mach = bfd_mach_amdgcn_gfx906; break;
-            case GFX (9, 0, 8): mach = bfd_mach_amdgcn_gfx908; break;
-            case GFX (9, 0, 10): mach = bfd_mach_amdgcn_gfx90a; break;
-            case GFX (10, 1, 0): mach = bfd_mach_amdgcn_gfx1010; break;
-            case GFX (10, 1, 1): mach = bfd_mach_amdgcn_gfx1011; break;
-            case GFX (10, 1, 2): mach = bfd_mach_amdgcn_gfx1012; break;
-            case GFX (10, 3, 0): mach = bfd_mach_amdgcn_gfx1030; break;
-            case GFX (10, 3, 1): mach = bfd_mach_amdgcn_gfx1031; break;
-            default:  mach = bfd_mach_amdgcn_unknown; break;
-            }
-#undef GFX
-        }
-
-      ptr += 12 + ((namesz + 3) & ~3) + ((descsz + 3) & ~3);
-    }
-
-
-  if (buffer != NULL)
-    free (buffer);
-
-  return mach;
-}
-
 static bool
 elf64_amdgcn_object_p (bfd *abfd)
 {
+  Elf_Internal_Ehdr *hdr = elf_elfheader (abfd);
   unsigned int mach;
+  unsigned char osabi_version;
 
-  if (elf_elfheader (abfd)->e_ident[EI_OSABI] != ELFOSABI_AMDGPU_HSA)
+  /* We should not get here if the OS ABI is not HSA, since we define
+     ELF_OSABI below.  */
+  BFD_ASSERT (hdr->e_ident[EI_OSABI] == ELFOSABI_AMDGPU_HSA);
+
+  /* We only support HSA code objects v3 and above.  */
+  osabi_version = hdr->e_ident[EI_ABIVERSION];
+  if (osabi_version < ELFABIVERSION_AMDGPU_HSA_V3)
     return false;
 
-  if (elf_elfheader (abfd)->e_ident[EI_ABIVERSION] < 1)
-    mach = bfd_amdgcn_get_mach_from_notes (abfd);
-  else
-    mach = elf_elfheader (abfd)->e_flags & EF_AMDGPU_MACH;
-
+  /* Read the specific processor model from e_flags.  */
+  mach = elf_elfheader (abfd)->e_flags & EF_AMDGPU_MACH;
   bfd_default_set_arch_mach (abfd, bfd_arch_amdgcn, mach);
+
   return true;
 }
 
