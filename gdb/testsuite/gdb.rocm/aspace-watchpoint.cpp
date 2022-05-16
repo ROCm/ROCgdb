d7b16816b218a084d986a97435b7d533a7a2ca0c
Zoran Zaric <zoran.zaric@amd.com>
2022-05-16 19:58:46 +0100

    Add test for watchpoints in default and private_lane address spaces
    This change introduces a simple test for watchpoints in default and
    private_lane address spaces.  The idea is that a variable array is
    expected to be allocated in a private_lane memory and should also have
    an alias address in the default address space (global memory).
    
    There are a few scenarios that this test is trying to cover:
     - watchpoint on a variable name (array) in a private_lane address
       space that gets triggered in the original context (frame, thread and
       lane),
     - watchpoint on the expression that describes an address of a single
       array element that gets triggered in the same context,
     - watchpoint on a single array element address directly that gets
       triggered under the context the address is bound to,
     - watchpoint that gets triggered in the callee function for all three
       previous scenarios,
     - watchpoint on a valid private_lane address set in a callee function
       but expected to get triggered in a caller function because the
       context of that address is still valid,
     - watchpoint on a kernel argument that is a pointer to the default
       address space that gets triggered in both callee and caller
       functions and
     - watchpoint on an address in a default address space directly that
       gets triggered in both callee and caller functions.
    
    Current restriction for more test scenarios is with the missing address
    space information for the pointers to the generic address space.  The
    idea would be to test if inserting a watchpoint on a value of a callee
    argument directly, would still trigger from a write to the same address
    on a caller side.
    
    Change-Id: Id807ffb8bd0f1c2827f15cfd3ca7a37d40558f60
    (cherry picked from commit 76cc12ad7ff12f38f8d4517215db5281cc959d25)

---
+++ gdb/testsuite/gdb.rocm/aspace-watchpoint.cpp
@@ -0,0 +1,84 @@
+/* Copyright (C) 2022-2023 Free Software Foundation, Inc.
+   Copyright (C) 2022-2023 Advanced Micro Devices, Inc. All rights reserved.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   Test writes to default address space (global memory) as well as
+   private_lane address space to test the watchpoints in different
+   address spaces.
+
+   The __syncthreads intrinsic call is used to force out a watchpoint
+   hit event being reported by the target on specific synchronisation
+   points for a more controlled execution.  */
+
+#include <hip/hip_runtime.h>
+
+__device__ void
+change_memory (char *private_ptr, int *global_ptr)
+{
+  /* Test if an address context (thread and SIMD lane) is handled
+     correctly depending on the address space that that address
+     belongs to.  */
+  *(private_ptr + 3) = 'c';
+  __syncthreads ();
+
+  *private_ptr = 'c';
+  __syncthreads ();
+
+  *global_ptr = 2;
+  __syncthreads ();
+}
+
+/* Kernel entry point.  Initialise and change the addresses in different
+   address spaces to trigger hardware watchpoints.  */
+__global__ void
+kernel (int *global_ptr)
+{
+  /* Initialise an array in the private_lane address space.  */
+  char array[4] = {'a', 'a', 'a', 'a'};
+
+  /* Only testing watchpoints in global and private_lane address
+     spaces.  */
+  array[3] = 'b';
+  __syncthreads ();
+
+  array[0] = 'b';
+  __syncthreads ();
+
+  *global_ptr = 1;
+  __syncthreads ();
+
+  change_memory (&array[0], global_ptr);
+
+  array[3] = 'd';
+  __syncthreads ();
+
+  *global_ptr = 3;
+  __syncthreads ();
+}
+
+int
+main (int argc, char* argv[])
+{
+  int *global_ptr;
+  hipMalloc (&global_ptr, 4);
+  int init_value_h = 0;
+  hipMemcpy (global_ptr, &init_value_h, sizeof (init_value_h), hipMemcpyHostToDevice);
+
+  kernel<<<1, 1, 0>>> (global_ptr);
+  hipDeviceSynchronize ();
+  return 0;
+}
