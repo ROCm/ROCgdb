18ced01243c8909527f164cf39f6571c109b3580
Laurent Morichetti <laurent.morichetti@amd.com>
2022-07-22 09:30:56 -0700

    Fix invalid cast to amdgpu_gdbarch_tdep
    In amd_dbgapi_target::fetch_registers and amd_dbgapi_target::store_registers,
    we should not get the amdgpu_gdbarch_tdep until after we know that the
    architecture is amdgpu.
    
    Change-Id: Iae7ec24c9f03c88e43be1eb7bff2c44547a34ee0

--- gdb/amd-dbgapi-target.c
+++ gdb/amd-dbgapi-target.c
@@ -1898,7 +1898,6 @@ void
 amd_dbgapi_target::fetch_registers (struct regcache *regcache, int regno)
 {
   struct gdbarch *gdbarch = regcache->arch ();
-  amdgpu_gdbarch_tdep *tdep = get_amdgpu_gdbarch_tdep (gdbarch);
 
   /* delegate to the host routines when not on the device */
 
@@ -1908,8 +1907,8 @@ amd_dbgapi_target::fetch_registers (struct regcache *regcache, int regno)
       return;
     }
 
+  amdgpu_gdbarch_tdep *tdep = get_amdgpu_gdbarch_tdep (gdbarch);
   amd_dbgapi_wave_id_t wave_id = get_amd_dbgapi_wave_id (regcache->ptid ());
-
   gdb_byte raw[AMDGPU_MAX_REGISTER_SIZE];
 
   amd_dbgapi_status_t status
@@ -1932,8 +1931,6 @@ void
 amd_dbgapi_target::store_registers (struct regcache *regcache, int regno)
 {
   struct gdbarch *gdbarch = regcache->arch ();
-  amdgpu_gdbarch_tdep *tdep = get_amdgpu_gdbarch_tdep (gdbarch);
-  gdb_byte raw[AMDGPU_MAX_REGISTER_SIZE];
 
   if (!is_amdgpu_arch (gdbarch))
     {
@@ -1941,9 +1938,10 @@ amd_dbgapi_target::store_registers (struct regcache *regcache, int regno)
       return;
     }
 
-  amd_dbgapi_wave_id_t wave_id = get_amd_dbgapi_wave_id (regcache->ptid ());
-
+  gdb_byte raw[AMDGPU_MAX_REGISTER_SIZE];
   regcache->raw_collect (regno, &raw);
+
+  amdgpu_gdbarch_tdep *tdep = get_amdgpu_gdbarch_tdep (gdbarch);
 
   /* If the register has read-only bits, invalidate the value in the regcache
      as the value actualy written may differ.  */
@@ -1959,6 +1957,8 @@ amd_dbgapi_target::store_registers (struct regcache *regcache, int regno)
     for (size_t r = 0; r < tdep->register_properties.size (); ++r)
       if (tdep->register_properties[r] & AMD_DBGAPI_REGISTER_PROPERTY_VOLATILE)
 	regcache->invalidate (r);
+
+  amd_dbgapi_wave_id_t wave_id = get_amd_dbgapi_wave_id (regcache->ptid ());
 
   amd_dbgapi_status_t status
     = amd_dbgapi_write_register (wave_id, tdep->register_ids[regno], 0,
