ROCm/ROCgdb

commit a2d1311a9b362cf177e059071f6ae8bab29dd38b

gdb/amd-dbgapi: remove amd_dbgapi_code_object_list_updated observer, call handle_solib_event directly
Replace the amd_dbgapi_code_object_list_updated observable with a
slightly more standard approach of dealing with solib events.

The amd_dbgapi_code_object_list_updated observable is triggered when a
host thread hits the special breakpoint inserted in the ROCm runtime.
This prompts solib-rocm to fetch a fresh list of code objects from
amd-dbgapi and update the list of solibs.  The observable is nice
because it decouples the amd-dbgapi target from the solib-rocm solib
provider (amd-dbgapi doesn't explicitly know about solib-rocm).  But
there's another way to achieve this, perhaps a bit more standard in GDB,
which is to call handle_solib_event.  handle_solib_event calls the
inferior solib_ops's handle_event method, where we can look for new code
objects.

Change-Id: If581b75dfa2acbb34cf37c723466366cd178d176

gdb/solib-rocm.c
@@ -100,6 +100,22 @@ rocm_solib_relocate_section_addresses (struct so_list *so,
   sec->endaddr = sec->endaddr + li->l_addr;
 }
 
+static void rocm_update_solib_list ();
+
+static void
+rocm_solib_handle_event ()
+{
+  /* Since we sit on top of svr4_so_ops, we might get called following an event
+     concerning host libraries.  We must therefore forward the call.  If the
+     event was for a ROCm code object, it will be a no-op.  On the other hand,
+     if the event was for host libraries, rocm_update_solib_list will be
+     essentially be a noop (it will reload the same code object list as was
+     previously loaded).  */
+  svr4_so_ops.handle_event ();
+
+  rocm_update_solib_list ();
+}
+
 /* Make a deep copy of the solib linked list.  */
 
 static struct so_list *
@@ -612,6 +628,7 @@ rocm_update_solib_list ()
       rocm_solib_ops.bfd_open = rocm_solib_bfd_open;
       rocm_solib_ops.relocate_section_addresses
 	= rocm_solib_relocate_section_addresses;
+      rocm_solib_ops.handle_event = rocm_solib_handle_event;
 
       /* Engage the ROCm so_ops.  */
       set_solib_ops (target_gdbarch (), &rocm_solib_ops);
@@ -636,10 +653,6 @@ extern initialize_file_ftype _initialize_rocm_solib;
 void
 _initialize_rocm_solib ()
 {
-  /* Install our observers.  */
-  amd_dbgapi_code_object_list_updated.attach (rocm_update_solib_list,
-					      "solib-rocm");
-
   /* FIXME: remove this when we can clear the solist in
      rocm_solib_create_inferior_hook.  */
   gdb::observers::inferior_created.attach (rocm_solib_target_inferior_created,

gdb/amd-dbgapi-target.c
@@ -66,12 +66,6 @@
 /* Big enough to hold the size of the largest register in bytes.  */
 #define AMDGPU_MAX_REGISTER_SIZE 256
 
-#define DEFINE_OBSERVABLE(name) decltype (name) name (#name)
-
-DEFINE_OBSERVABLE (amd_dbgapi_code_object_list_updated);
-
-#undef DEFINE_OBSERVABLE
-
 /* amd-dbgapi-specific inferior data.  */
 
 struct amd_dbgapi_inferior_info
@@ -1497,7 +1491,21 @@ process_one_event (amd_dbgapi_event_id_t event_id,
       }
 
     case AMD_DBGAPI_EVENT_KIND_CODE_OBJECT_LIST_UPDATED:
-      amd_dbgapi_code_object_list_updated.notify ();
+      /* We get here when the following sequence of events happens:
+
+	   - the inferior hits the amd-dbgapi "r_brk" internal breakpoint
+	   - amd_dbgapi_target_breakpoint::check_status calls
+	     amd_dbgapi_report_breakpoint_hit, which queues an event of this
+	     kind in dbgapi
+	   - amd_dbgapi_target_breakpoint::check_status calls
+	     process_event_queue, which pulls the event out of dbgapi, and
+	     gets us here
+
+	 When amd_dbgapi_target_breakpoint::check_status is called, the current
+	 inferior is the inferior that hit the breakpoint, which should still be
+	 the case now.  */
+      gdb_assert (inf == current_inferior ());
+      handle_solib_event ();
       break;
 
     case AMD_DBGAPI_EVENT_KIND_BREAKPOINT_RESUME:

gdb/amd-dbgapi-target.h
@@ -22,7 +22,6 @@
 #define AMD_DBGAPI_TARGET_H 1
 
 #include "gdbsupport/common-defs.h"
-#include "gdbsupport/observable.h"
 #include "gdbsupport/traits.h"
 
 #include <amd-dbgapi/amd-dbgapi.h>
@@ -65,10 +64,6 @@ operator!= (const T &lhs, const T &rhs)
 {
   return !(lhs == rhs);
 }
-
-/* AMD debugger API event observers.  */
-
-extern gdb::observers::observable<> amd_dbgapi_code_object_list_updated;
 
 /* Return true if the given ptid is a GPU thread (wave) ptid.  */
 

commit a19e14755e7e97bee133bc790d1bef7eef6b0541

gdb/solib-rocm: fix regression on catch load
Commit a2d1311a9b3 (gdb/amd-dbgapi: remove
amd_dbgapi_code_object_list_updated observer, call handle_solib_event
directly) introduced a regression on catch load.  This was exposed by
the gdb.threads/check-libthread-db.exp and gdb.base/catch-load.exp
tests.

The issue is as follows.  Under handle_solib_event, the new
implementation calls rocm_solib_handle_event, which in turns calls
rocm_update_solib_list.  This call updates the per-inferior solib list
that solib-rocm maintains:

  current_inferior ()->pspace->clear_solib_cache ();
  solib_add (NULL, 0, auto_solib_add);

The first call clears current_inferior ()->pspace->added_solibs, and
then solib_add detects new solibs and adds them to current_inferior
()->pspace->added_solibs.

Once the call is finished and control flow returns to
handle_solib_event, the procedure does the same clear_solib_cache -
solib_add dance.  While doing that, the clear_solib_cache removes the
knowledge about which solibs have been detected in the previous
solib_add call.  The new solib_add call detects nothing new, leaving the
added_solibs empty.

Later, when processing solib_catchpoint::check_status, we see that
added_solibs is empty and deduce from that that execution does not need
to stop.

The core of the problem is that rocm_solib_handle_event (and
rocm_update_solib_list under it) should not call solib_add.  This
method should only retrieve list of shared objects from the rocm runtime
and update solib-rocm's solib list  accordingly.

Later when the control returns to handle_solib_event, it will call
add_solib, which will in turn will call `gdbarch_so_ops (target_gdbarch
())->current_sos ()` to retrieve the list of shared objects refreshed
during handle_event.

This patch does this change.

It turns out that rocm_update_solib_list is also called from
rocm_solib_target_inferior_created.  This is to detect device codes
already present on the device during attach.  As rocm_update_solib_list

gdb/solib-rocm.c
@@ -610,13 +610,6 @@ rocm_update_solib_list ()
 
   xfree (code_object_list);
 
-  /* Force GDB to reload the solibs.  */
-  current_inferior ()->pspace->clear_solib_cache ();
-
-  /* Switch terminal for any messages produced by
-     breakpoint_re_set.  */
-  target_terminal::ours_for_output ();
-
   if (rocm_solib_ops.current_sos == NULL)
     {
       /* Override what we need to */
@@ -632,11 +625,6 @@ rocm_update_solib_list ()
       /* Engage the ROCm so_ops.  */
       set_gdbarch_so_ops (target_gdbarch (), &rocm_solib_ops);
     }
-
-  solib_add (NULL, 0, auto_solib_add);
-
-  /* Switch it back.  */
-  target_terminal::inferior ();
 }
 
 static void
@@ -644,6 +632,10 @@ rocm_solib_target_inferior_created (inferior *inf)
 {
   rocm_free_solib_list (get_solib_info (inf));
   rocm_update_solib_list ();
+
+  /* Force GDB to reload the solibs.  */
+  current_inferior ()->pspace->clear_solib_cache ();
+  solib_add (nullptr, 0, auto_solib_add);
 }
 
 /* -Wmissing-prototypes */
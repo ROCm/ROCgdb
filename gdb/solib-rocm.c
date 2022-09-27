a19e14755e7e97bee133bc790d1bef7eef6b0541
Lancelot SIX <lancelot.six@amd.com>
2022-09-27 14:20:40 +0100

    gdb/solib-rocm: fix regression on catch load
    Commit a2d1311a9b3 (gdb/amd-dbgapi: remove
    amd_dbgapi_code_object_list_updated observer, call handle_solib_event
    directly) introduced a regression on catch load.  This was exposed by
    the gdb.threads/check-libthread-db.exp and gdb.base/catch-load.exp
    tests.
    
    The issue is as follows.  Under handle_solib_event, the new
    implementation calls rocm_solib_handle_event, which in turns calls
    rocm_update_solib_list.  This call updates the per-inferior solib list
    that solib-rocm maintains:
    
      current_inferior ()->pspace->clear_solib_cache ();
      solib_add (NULL, 0, auto_solib_add);
    
    The first call clears current_inferior ()->pspace->added_solibs, and
    then solib_add detects new solibs and adds them to current_inferior
    ()->pspace->added_solibs.
    
    Once the call is finished and control flow returns to
    handle_solib_event, the procedure does the same clear_solib_cache -
    solib_add dance.  While doing that, the clear_solib_cache removes the
    knowledge about which solibs have been detected in the previous
    solib_add call.  The new solib_add call detects nothing new, leaving the
    added_solibs empty.
    
    Later, when processing solib_catchpoint::check_status, we see that
    added_solibs is empty and deduce from that that execution does not need
    to stop.
    
    The core of the problem is that rocm_solib_handle_event (and
    rocm_update_solib_list under it) should not call solib_add.  This
    method should only retrieve list of shared objects from the rocm runtime
    and update solib-rocm's solib list  accordingly.
    
    Later when the control returns to handle_solib_event, it will call
    add_solib, which will in turn will call `gdbarch_so_ops (target_gdbarch
    ())->current_sos ()` to retrieve the list of shared objects refreshed
    during handle_event.
    
    This patch does this change.
    
    It turns out that rocm_update_solib_list is also called from
    rocm_solib_target_inferior_created.  This is to detect device codes
    already present on the device during attach.  As rocm_update_solib_list
    does not call solib_add anymore, this patch adds such call in
    rocm_solib_target_inferior_created instead.
    
    As rocm_solib_target_inferior_created is called by post_create_inferior
    (via gdb::observers::inferior_created.notify), and given that
    post_create_inferior calls target_terminal::ours_for_output,
    rocm_solib_target_inferior_created does not need to juggle terminal
    settings as it was in rocm_update_solib_list.
    
    Tested on x86_64 + gfx900.
    
    Change-Id: I349c1713fed61b67c36aee4e4a1ad09eb04cb0c8

--- gdb/solib-rocm.c
+++ gdb/solib-rocm.c
@@ -610,13 +610,6 @@ rocm_update_solib_list ()
 
   xfree (code_object_list);
 
-  /* Force GDB to reload the solibs.  */
-  current_inferior ()->pspace->clear_solib_cache ();
-
-  /* Switch terminal for any messages produced by
-     breakpoint_re_set.  */
-  target_terminal::ours_for_output ();
-
   if (rocm_solib_ops.current_sos == NULL)
     {
       /* Override what we need to */
@@ -632,11 +625,6 @@ rocm_update_solib_list ()
       /* Engage the ROCm so_ops.  */
       set_gdbarch_so_ops (target_gdbarch (), &rocm_solib_ops);
     }
-
-  solib_add (NULL, 0, auto_solib_add);
-
-  /* Switch it back.  */
-  target_terminal::inferior ();
 }
 
 static void
@@ -644,6 +632,10 @@ rocm_solib_target_inferior_created (inferior *inf)
 {
   rocm_free_solib_list (get_solib_info (inf));
   rocm_update_solib_list ();
+
+  /* Force GDB to reload the solibs.  */
+  current_inferior ()->pspace->clear_solib_cache ();
+  solib_add (nullptr, 0, auto_solib_add);
 }
 
 /* -Wmissing-prototypes */
