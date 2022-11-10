32d0555d8341ceef79b5d9f858c6fa5199a1b6bc
Pedro Alves <pedro@palves.net>
2022-11-10 10:46:35 +0000

    Test debugging a program that branches to a bad address
    Add a test that debugs a program that branches to a bad address.
    Check that when GDB catches the fault, the PC points to the branch
    destination address.
    
    E.g.:
    
     (gdb) c
     Thread 6 "branch-fault" received signal SIGSEGV, Segmentation fault.
     [Switching to thread 6, lane 0 (AMDGPU Lane 1:2:1:1/0 (0,0,0)[0,0,0])]
     0x0000000000000100 in ?? ()
     (gdb)
     p $pc
     $1 = (void (*)(void)) 0x100
    
    Change-Id: I6ddf7dc51e39d4b0fdb8c90d207d1ed8ee9705cd

---
+++ gdb/testsuite/gdb.rocm/branch-fault.cpp
@@ -0,0 +1,35 @@
+/* Copyright (C) 2022 Advanced Micro Devices, Inc. All rights reserved.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <hip/hip_runtime.h>
+
+typedef void (*func_ptr) ();
+
+__global__ void
+kernel ()
+{
+  func_ptr bad_func_ptr = (func_ptr) 0x100;
+  bad_func_ptr ();
+}
+
+int
+main (int argc, char* argv[])
+{
+  hipLaunchKernelGGL (kernel, dim3 (1), dim3 (1), 0, 0);
+  hipDeviceSynchronize ();
+  return 0;
+}
