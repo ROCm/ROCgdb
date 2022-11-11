4e018803d68587f9c3f89c3e295fb14ce431991b
Lancelot SIX <lancelot.six@amd.com>
2022-11-11 17:27:12 -0500

    gdb/testsuite: Add test for non-stop displaced stepping
    This patch adds a regression test for rocm-dbgapi.  In some version, an
    assertion was failing when trying to allocate a displaced buffer for a
    wave while another wave of the same dispatch was running.
    
    This got fixed in rocm-dbgapi in 709b484 (Relax the assertion that the
    queue must be suspended to inspect the context save user header).
    
    Tested on x86_64 + gfx1030.
    
    Change-Id: I14307e236d9814676b76a80a13f1b65aeb72b7b2
    (cherry picked from commit ced9be302e68de33a3862facc51186841b8eb3d6)

---
+++ gdb/testsuite/gdb.rocm/nonstop-displaced.cpp
@@ -0,0 +1,49 @@
+/* Copyright (C) 2022-2023 Advanced Micro Devices, Inc. All rights reserved.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <hip/hip_runtime.h>
+
+__device__ void
+foo ()
+{
+}
+
+__device__ static bool lock = true;
+
+__global__ void
+kern ()
+{
+  while (__hip_atomic_load (&lock, __ATOMIC_RELAXED,
+			    __HIP_MEMORY_SCOPE_AGENT))
+    {
+      if (blockIdx.x == 0)
+	{
+	  foo ();
+	  __hip_atomic_store (&lock, false, __ATOMIC_RELAXED,
+			      __HIP_MEMORY_SCOPE_AGENT);
+	}
+      __builtin_amdgcn_s_sleep (1);
+    }
+}
+
+int
+main (int argc, char *argv[])
+{
+  kern<<<2, 1>>> ();
+  hipDeviceSynchronize ();
+  return 0;
+}
