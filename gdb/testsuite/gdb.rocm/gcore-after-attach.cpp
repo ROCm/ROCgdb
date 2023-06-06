6b547452ac1faf0f58f98ea425f1f42df7646c3a
Lancelot SIX <lancelot.six@amd.com>
2023-06-06 17:20:31 +0000

    gdb/testsuite: Add gdb.rocm/gcore-after-attach test
    On some AMDGPU architectures, waves submitted by a process not being
    debugged have some trap temporary registers left uninitialized.  When
    GDB attaches to such process, it cannot retrieve some information about
    those waves, such as wave's coordinates in the dispatch.  Waves
    submitted after GDB has attached have the trap temporary registers
    initialized.
    
    If later GDB generates a core dump of the process, it can dump the state
    of waves with trap temporary registers initialized and some with trap
    temporary registers not initialized.
    
    This patch adds a testcase which ensures that GDB can recognize a non
    fully initialized wave from a core dump, and does not accidentally
    treats it as an initialized wave.
    
    Change-Id: I461c2a63026e555c200aee958774efc0f348ea00

---
+++ gdb/testsuite/gdb.rocm/gcore-after-attach.cpp
@@ -0,0 +1,47 @@
+/* Copyright (C) 2023 Free Software Foundation, Inc.
+   Copyright (C) 2023 Advanced Micro Devices, Inc. All rights reserved.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <hip/hip_runtime.h>
+#include <vector>
+
+#define CHECK(cmd)                                                           \
+  do                                                                         \
+    {                                                                        \
+      hipError_t error = cmd;                                                \
+      if (error != hipSuccess)                                               \
+	{                                                                    \
+	  fprintf (stderr, "error: '%s'(%d) at %s:%d\n",                     \
+		   hipGetErrorString (error), error, __FILE__, __LINE__);    \
+	  exit (EXIT_FAILURE);                                               \
+	}                                                                    \
+    } while (0)
+
+__global__ void
+kern ()
+{
+  while (1)
+    __builtin_amdgcn_s_sleep (1);
+}
+
+int
+main (int argc, char* argv[])
+{
+  kern<<<1, 1>>> ();
+  CHECK (hipDeviceSynchronize ());
+}
+
