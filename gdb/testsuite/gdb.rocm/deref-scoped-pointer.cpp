25ed5df891fd969261740c526aea72f90a48f693
Pedro Alves <pedro@palves.net>
2023-10-23 13:51:00 +0100

    Test dereferencing scoped pointers from different contexts
    Dereferencing a pointer to private_lane memory (a pointer whose target
    address has "lane" scope) should yield the private_lane memory
    contents of the currently selected lane.  This is how the HIP language
    works, and so GDB should behave the same, irrespective of whether the
    pointer's target address is a generic address or a private_lane
    address, or whether the pointer exists in the program or is in a GDB
    convenience variable.
    
    The printf's in the test program are there just to confirm how HIP
    behaves without debugger intervention.  They currently print (the
    address may be different):
    
     global_ptr=0x2000000000008, *global_ptr=0
     global_ptr=0x2000000000008, *global_ptr=1
    
    So from GDB, dereferencing *global_ptr should yield the same values.
    E.g.:
    
      (gdb) lane apply all print *global_ptr
      Lane 0 (AMDGPU Lane 1:4:1:1/0 (0,0,0)[0,0,0]):
      $1 = 0
      Lane 1 (AMDGPU Lane 1:4:1:1/1 (0,0,0)[1,0,0]):
      $2 = 1
    
    You do see the above if "global_ptr" is set to point at "local_var" by
    GDB itself, with "global_ptr = &local_var".  If however, you try to
    dereference the "global_ptr" unmodified as set by the program, at a
    time it holds a generic address, then GDB fails with:
    
      print *global_ptr
      Cannot access memory at address 0x2000000000008
    
    Also, currently what GDB writes to the generic address pointer is
    incorrect, it writes the internal representation of the lane address.
    So letting execution continue after such a write runs into undefined
    behavior.
    
    For these reasons, some parts of the test are kfailed for now.  This
    should improve later when the address spaces support rework is
    completed.
    
    Change-Id: Id8e6e78fb8ea4a3e963624ebee47a90337454aa7

---
+++ gdb/testsuite/gdb.rocm/deref-scoped-pointer.cpp
@@ -0,0 +1,82 @@
+/* Copyright 2023 Free Software Foundation, Inc.
+   Copyright (C) 2023 Advanced Micro Devices, Inc. All rights reserved.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <hip/hip_runtime.h>
+#include <stdio.h>
+
+#define CHECK(cmd)							\
+  {									\
+    hipError_t error = cmd;						\
+    if (error != hipSuccess)						\
+      {									\
+	fprintf (stderr, "error: '%s'(%d) at %s:%d\n",			\
+		 hipGetErrorString (error), error, __FILE__, __LINE__);	\
+	exit (EXIT_FAILURE);						\
+      }									\
+  }
+
+__device__ static void
+done ()
+{
+}
+
+/* This is initialized by the kernel function below.  */
+__device__ size_t *global_ptr = nullptr;
+
+/* This is initialized by GDB.  */
+__device__ size_t *global_ptr2 = nullptr;
+
+__global__ void
+kernel ()
+{
+  size_t local_var = blockIdx.x * blockDim.x + threadIdx.x;
+
+  /* This will be zero for the first HIP thread, or lane.  */
+  if (local_var == 0)
+    global_ptr = &local_var;
+
+  /* This is just to confirm how dereferencing the pointer from
+     different lanes yield a different value.  GDB should behave the
+     same.  */
+  assert (global_ptr != nullptr);
+  printf ("global_ptr=%p, *global_ptr=%lx\n", global_ptr, *global_ptr);
+
+  done (); /* set breakpoint here */
+
+  /* Convenience nullptr check useful if you run the program manually
+     or skip part of the testcase.  */
+  if (global_ptr2 != nullptr)
+    {
+      printf ("global_ptr2=%p, *global_ptr2=%lx\n", global_ptr2, *global_ptr2);
+      /* GDB made global_ptr2 point to &local_var too.  If GDB wrote
+	 to global_ptr2 correctly, this should pass.  */
+      assert (global_ptr == global_ptr2);
+    }
+  else
+    printf ("global_ptr2=nullptr\n");
+
+}
+
+int
+main ()
+{
+  /* We only need more than one lane.  Two is sufficient.  */
+  kernel<<<dim3 (1), dim3 (2)>>> ();
+  CHECK (hipDeviceSynchronize ());
+  return 0;
+}
