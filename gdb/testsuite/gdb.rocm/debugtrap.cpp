bbf582bb2f70502b8a75ccf6925783f783bb77db
Lancelot SIX <lancelot.six@amd.com>
2023-11-09 10:50:14 +0000

    gdb/testsuite/gdb.rocm: Test __builtin_debugtrap (a.k.a. "s_trap 3")
    On AMDGPU target, the "s_trap 3" instruction (generated by the
    __builtin_debugtrap intrinsic) is expected to enter the trap handler,
    and either return if the program is not executing under a debugger or
    cause a SIGTRAP if executing under the debugger[1].
    
    This patch adds a test ensuring that the debugger is capable of
    triggering this behavior, and that we can resume execution after the
    SIGTRAP has been seen.
    
    [1] https://llvm.org/docs/AMDGPUUsage.html#trap-handler-abi
    
    Change-Id: I6b2c8fea8788b8f61869624416c887ca6e72da2c

---
+++ gdb/testsuite/gdb.rocm/debugtrap.cpp
@@ -0,0 +1,45 @@
+/* Copyright 2023-2024 Free Software Foundation, Inc.
+   Copyright (C) 2023-2024 Advanced Micro Devices, Inc. All rights reserved.
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <hip/hip_runtime.h>
+#include <cstdlib>
+
+#define CHECK(cmd)							\
+  {									\
+    hipError_t error = cmd;						\
+    if (error != hipSuccess)						\
+      {									\
+	fprintf (stderr, "error: '%s'(%d) at %s:%d\n",			\
+		 hipGetErrorString (error), error, __FILE__, __LINE__);	\
+	exit (EXIT_FAILURE);						\
+      }									\
+  }
+
+__global__ void
+kern ()
+{
+  __builtin_debugtrap ();
+}
+
+int
+main (int argc, char **argv)
+{
+  kern<<<1, 1>>> ();
+  CHECK (hipDeviceSynchronize ());
+  return 0;
+}
