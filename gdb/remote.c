9547032d1a38a6bb12ae1e8486f7a6af55666a89
Rohr, Stephan <stephan.rohr@intel.com>
2024-02-12 00:02:24 -0800

    gdb, remote: Skip sending of 'vCont' packet if remote target is empty
    If the remote target has no threads that need to be stopped, we can
    skip sending the 'vCont' package in 'remote_stop_ns'.
    
    Approved-By: Tom Tromey <tom@tromey.com>

--- gdb/remote.c
+++ gdb/remote.c
@@ -7884,6 +7884,10 @@ remote_target::remote_stop_ns (ptid_t ptid)
 	  }
       }
 
+  /* Skip 'vCont' packet if there are no threads to be stopped.  */
+  if (all_non_exited_threads (this, ptid).empty ())
+    return;
+
   if (!rs->supports_vCont.t)
     error (_("Remote server does not support stopping threads"));
 
