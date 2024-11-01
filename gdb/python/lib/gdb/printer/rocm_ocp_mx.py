f9eac22c991bcc152e7c737630cfab903667f486
Olu Ogunbowale <oogunbow@amd.com>
2024-11-01 21:30:48 +0000

    Add support for ROCm microscaling floating point types
    printer/rocm_ocp_mx.py: Add following pretty printer support for ROCm
    fp8/6/4 types:
    
        - fp4x2_e2m1
        - fp6x32_e2m3
        - fp6x32_e3m2
        - fp8_e4m3
        - fp8_e5m2
        - fp8x2_e4m3
        - fp8x2_e5m2
        - uint8_e8m0
    
    function/rocm_get_scaled_float.py: Add $_mx_get_scaled_float function
    for ROCm microscaling objects.
    
    Introduce the `$_mx_get_scaled_float` GDB function to retrieve scaled
    values of ROCm microscaling scalar objects. This function supports
    extracting values from microscaling types and applying an optional
    scale factor. This simplifies debugging of ROCm microscaling types by
    providing a direct method to obtain their scaled floating-point
    representations.
    
    gdb.rocm/ocp_mx.{exp/cpp}: Add tests to ensure ROCm fp8/6/4 pretty
    printers and $_mx_get_scaled_float work correctly.
    
    Feature: SWDEV-449819
    Change-Id: I5acca3bfff4d293d6d8b7f70a32885bb620c0bc2
    (cherry picked from commit ad1525387e2ae0a7e46d5b828967ab092c63ecfd)

---
+++ gdb/python/lib/gdb/printer/rocm_ocp_mx.py
@@ -0,0 +1,47 @@
+# Pretty-printers for ROCm OCP microscaling floating point types
+# Copyright (C) 2025 Free Software Foundation, Inc.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import gdb
+import gdb.printing
+from gdb.function.rocm_get_scaled_float import *
+
+
+class OcpMxFpValuePrinter(gdb.ValuePrinter):
+    def __init__(self, val):
+        self._val = val
+
+    def to_string(self):
+        fp = extract_mx_values(self._val)
+        return "{{{}}}".format(", ".join([str(f) for f in fp]))
+
+
+class OcpMxScaleValuePrinter(gdb.ValuePrinter):
+    def __init__(self, val):
+        self._val = val
+
+    def to_string(self):
+        return compute_uint8e8m0(self._val)
+
+
+def build_ocp_mx_subprinters():
+    pp = gdb.printing.RegexpCollectionPrettyPrinter("rocm_ocp_mx")
+    pp.add_printer("hipext_ocp_fp", hipexp_ocp_fp_re(), OcpMxFpValuePrinter)
+    pp.add_printer("amd_scale_t", "^__amd_scale_t$", OcpMxScaleValuePrinter)
+    return pp
+
+
+# Register pretty printer collection automatically when the script is sourced by GDB
+gdb.printing.register_pretty_printer(gdb.current_objfile(), build_ocp_mx_subprinters())
