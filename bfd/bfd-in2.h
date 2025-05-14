bd5e13081270c0e4a3d7387f035f3b6ab6d5893e
Laurent Morichetti <laurent.morichetti@amd.com>
2025-05-14 12:35:23 -0400

    add support for gfx1150 & gfx1151
    Change-Id: I4d41b67dc57d38c646579f4f33c076a90496a7da
    (cherry picked from commit d40b2dbd3a806172199f57720254e3c70e1370c6)

--- bfd/bfd-in2.h
+++ bfd/bfd-in2.h
@@ -1846,6 +1846,8 @@ enum bfd_architecture
 #define bfd_mach_amdgcn_gfx1100         0x041
 #define bfd_mach_amdgcn_gfx1101         0x046
 #define bfd_mach_amdgcn_gfx1102         0x047
+#define bfd_mach_amdgcn_gfx1150         0x043
+#define bfd_mach_amdgcn_gfx1151         0x04a
 #define bfd_mach_amdgcn_gfx12_generic   0x059
 #define bfd_mach_amdgcn_gfx1200         0x048
 #define bfd_mach_amdgcn_gfx1201         0x04e
