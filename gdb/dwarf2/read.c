c6c03a11dfe4acd6b40b351f25c2e75b13f6315c
Shahab Vahedi <shahab.vahedi@amd.com>
2025-09-01 21:02:20 +0200

    gdb/dwarf2: add workaround for HIP language detection
    Currently, the HIP compiler emits DW_LANG_C_plus_plus_14 instead
    of DW_LANG_HIP for the DW_AT_language field.  To mitigate that,
    we check if the target architecture is "bfd_arch_amdgcn" when the
    DW_AT_language is set to C++, and then override the language to HIP.
    
    Change-Id: I65f40757bd33f7bdfeb59bf786aa2c1038cbcb63
    (cherry picked from commit e1ba71be55ebb0cfaf39fd3a4892e76ca21e840e)

--- gdb/dwarf2/read.c
+++ gdb/dwarf2/read.c
@@ -22065,6 +22065,26 @@ prepare_one_comp_unit (struct dwarf2_cu *cu, struct die_info *comp_unit_die,
     {
       lang = dwarf_lang_to_enum_language (attr->constant_value (0));
       dw_lang = (dwarf_source_language) attr->constant_value (0);
+
+      /* The HIP compiler instead of setting DW_AT_language to DW_LANG_HIP
+	 sets it to DW_LANG_C_plus_plus_14.  As a workaround for the
+	 language detection, we fall back to checking the target
+	 architecture when the language is C++.  There are still 2 caveats:
+
+	 1) Erroneously labeling device codes written purely in C++ as HIP
+	 2) Not detecting the host HIP codes as HIP, but the emitted C++14
+
+	 Point (1) is quite unlikely.  Point (2) can be remedied for now
+	 with a "set language hip" command in GDB itself.  Still, this
+	 workaround works better than checking the "producer", since
+	 various providers tend to name them slightly differently.  */
+      if (lang == language_cplus
+	  && (cu->per_objfile->per_bfd->obfd->arch_info->arch
+	      == bfd_arch_amdgcn))
+	{
+	  lang = language_hip;
+	  dw_lang = DW_LANG_HIP;
+	}
     }
   else
     lang = pretend_language;
