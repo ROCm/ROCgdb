087297525666d4010088355702e703e64d8e9f86
Pedro Alves <pedro@palves.net>
2025-09-19 18:49:33 +0100

    gdb.rocm/finish.exp: Improve on Windows
    On Windows, gdb.rocm/finish.exp shows 53 FAILs.  Most of them are
    caused by the .exp file setting a breakpoint on a function that does
    not exist:
    
     break returnSized<1ul> if $_lane == 3
     Function "returnSized<1ul>" not defined.
     Make breakpoint pending on future shared library load? (y or [n]) y
     Breakpoint 4 (returnSized<1ul>) pending.
     (gdb) continue
     Continuing.
     warning: Temporarily disabling breakpoints for unloaded shared library "memory://10284#offset=0x7ff63c5a3000&size=95392"
     [Thread 10284.0x2d44 exited with code 0]
     [Thread 10284.0x3b08 exited with code 0]
     [Thread 10284.0x1838 exited with code 0]
     [Thread 10284.0x1384 exited with code 0]
     [Thread 10284.0x198 exited with code 0]
     [Inferior 1 (process 10284) exited normally]
     (gdb) FAIL: gdb.rocm/finish.exp: structWithCharArray: size=1: continue to breakpoint: returnSized<1ul> (the program exited)
     ... many cascading fails from here ...
    
    "returnSized<1ul>" does not exist on Windows, we have instead instead
    "returnSized<1ull>", etc.:
    
      (gdb) b returnSized
      Function "returnSized" not defined.
      Make breakpoint pending on future shared library load? (y or [n]) y
      Breakpoint 1 (returnSized) pending.
      (gdb) r
      Starting program: C:\msys2\home\alves\gdb\build-testsuite\outputs\gdb.rocm\finish\finish
      ...
      Thread 7 "kernel" hit Breakpoint 1.1, with lanes [0-31], returnSized<1ull> () at C:/rocgdb/src/gdb/testsuite/gdb.rocm\finish.cpp:107
      107       for (int i = 0; i < N; ++i)
      (gdb) info break
      Num     Type           Disp Enb Address            What
      1       breakpoint     keep y   <MULTIPLE>
    	  breakpoint already hit 1 time
      1.1                         y   0x00000003000223e0 in returnSized<1ull>() at C:/rocgdb/src/gdb/testsuite/gdb.rocm\finish.cpp:107
      1.2                         y   0x00000003000228d4 in returnSized<2ull>() at C:/rocgdb/src/gdb/testsuite/gdb.rocm\finish.cpp:107
      1.3                         y   0x0000000300022dd4 in returnSized<4ull>() at C:/rocgdb/src/gdb/testsuite/gdb.rocm\finish.cpp:107
      1.4                         y   0x00000003000232d4 in returnSized<8ull>() at C:/rocgdb/src/gdb/testsuite/gdb.rocm\finish.cpp:107
      1.5                         y   0x00000003000237e8 in returnSized<9ull>() at C:/rocgdb/src/gdb/testsuite/gdb.rocm\finish.cpp:107
      1.6                         y   0x0000000300023d88 in returnSized<16ull>() at C:/rocgdb/src/gdb/testsuite/gdb.rocm\finish.cpp:107
      1.7                         y   0x00000003000243b4 in returnSized<32ull>() at C:/rocgdb/src/gdb/testsuite/gdb.rocm\finish.cpp:107
      1.8                         y   0x0000000300024b30 in returnSized<33ull>() at C:/rocgdb/src/gdb/testsuite/gdb.rocm\finish.cpp:107
      1.9                         y   0x0000000300025460 in returnSized<64ull>() at C:/rocgdb/src/gdb/testsuite/gdb.rocm\finish.cpp:107
      1.10                        y   0x0000000300026130 in returnSized<128ull>() at C:/rocgdb/src/gdb/testsuite/gdb.rocm\finish.cpp:107
      (gdb)
    
    This is simply because long is 32-bit on Window x64, and so size_t
    maps to unsigned long long.
    
    Fix this by using "int" instead of "size_t" for the template non-type
    parameters, as "int" does not need a suffix, and so this works the
    same everywhere.
    
    Change-Id: I40a70f2285752dd0da8b9da34d9fb98962783f7a

--- gdb/testsuite/gdb.rocm/finish.cpp
+++ gdb/testsuite/gdb.rocm/finish.cpp
@@ -34,7 +34,12 @@
 
 struct Empty {};
 
-template<typename T, size_t N>
+/* Note: we use "int" instead of "size_t" here and throughout so that
+   the N constant does not end up with suffixes in function
+   prototypes.  E.g., We get "returnSized<1>" instead
+   "returnSized<1ul>" or "returnSized<1ull>" depending on the
+   underlying type of size_t.  */
+template<typename T, int N>
 struct Custom
 {
   T data[N];
@@ -99,7 +104,7 @@ returnEmpty ()
   return {};
 }
 
-template<size_t N>
+template<int N>
 __device__ static Custom<char, N>
 returnSized ()
 {
@@ -110,12 +115,12 @@ returnSized ()
   return ret;
 }
 
-template<size_t N>
+template<int N>
 __device__ static Custom<Union, N>
 returnCustomWithUnion ()
 {
   Custom<Union, N> ret;
-  for (size_t i = 0; i < N; i++)
+  for (int i = 0; i < N; i++)
     ret.data[i].as_int = 0x61616161;
   return ret;
 }
