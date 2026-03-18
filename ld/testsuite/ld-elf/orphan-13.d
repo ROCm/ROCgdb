bd40fc073ae0c30f8ad40623e9f40da1be09b0e5
Eric Botcazou <ebotcazou@gcc.gnu.org>
2026-03-18 12:49:23 +0100

    ld/ELF: Fix small issue with orphan sections and NOLOAD output section
    What happens is that the effect of the command:
    
      .foo (NOLOAD) : {}
    
    is not equivalent to that of:
    
      .foo (NOLOAD) : { *(.foo) }
    
    when there is more than 1 object file containing a .foo section: the former
    will output two .foo sections, the first with PROGBITS and the second with
    NOBITS, where the latter will output only one with NOBITS.
    
    Given that the commands are essentially equivalent, the linker ought to
    yield the same outcome, namely the single output section with NOBITS.

---
+++ ld/testsuite/ld-elf/orphan-13.d
@@ -0,0 +1,12 @@
+#source: orphan-13a.s
+#source: orphan-13b.s
+#source: orphan-13c.s
+#ld: -T orphan-13.ld
+#readelf: -S --wide
+#xfail: [uses_genelf]
+#xfail: xstormy16-*-*
+
+#...
+  \[[ 0-9]+\] \.foo +NOBITS +[0-9a-f]+ +[0-9a-f]+ +0+30 +0+ +A +0 +0 +[0-9]+
+  \[[ 0-9]+\] [._][^f].*
+#pass
---
+++ ld/testsuite/ld-elf/orphan-13a.s
@@ -0,0 +1,14 @@
+ .globl main
+ .globl _main
+ .globl start
+ .globl _start
+ .globl __start
+ .text
+main:
+_main:
+start:
+_start:
+__start:
+
+ .section .foo,"a",%progbits
+ .long 1,1,1,1
