ROCm/ROCgdb

commit d19862435df418a2ec78d078048f54d115c67a66

gdbserver: require_running_or_break for the 'z' and 'vCont' packets
Similar to several other packages that access/modify process state,
the 'z'/'Z' and 'vCont' packets need to access the process.  Hence,
add a `require_running_or_break` as a pre-check.

Also add a test to check that gdbserver does not crash when certain
packets are received while there does not exist a process.

Approved-By: Tom Tromey <tom@tromey.com>

gdbserver/server.cc
@@ -3577,6 +3577,7 @@ handle_v_requests (char *own_buf, int packet_len, int *new_packet_len)
 
       if (startswith (own_buf, "vCont;"))
 	{
+	  require_running_or_return (own_buf);
 	  handle_v_cont (own_buf);
 	  return;
 	}
@@ -5035,6 +5036,7 @@ process_serial_event (void)
       /* Fallthrough.  */
     case 'z':  /* remove_ ... */
       {
+	require_running_or_break (cs.own_buf);
 	char *dataptr;
 	ULONGEST addr;
 	int kind;

gdb/testsuite/gdb.server/require-running.exp
@@ -1,0 +1,72 @@
+# Copyright 2025 Free Software Foundation, Inc.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Test that the server properly returns an error response for packets
+# that require the existence of a process.
+
+load_lib gdbserver-support.exp
+
+require allow_gdbserver_tests
+
+clean_restart
+
+# Make sure we're disconnected, in case we're testing with an
+# extended-remote board, therefore already connected.
+gdb_test "disconnect" ".*"
+
+set res [gdbserver_start "--multi" ""]
+set gdbserver_gdbport [lindex $res 1]
+gdb_assert {![gdb_target_cmd "extended-remote" $gdbserver_gdbport]} \
+    "gdbserver connected"
+
+gdb_test_no_output "set debug remote on"
+
+set packets \
+  { \
+    "C" \
+    "C00" \
+    "g" \
+    "G00" \
+    "Hg0" \
+    "m1234,1" \
+    "M1234,1:00" \
+    "qAttached" \
+    "qC" \
+    "qCRC:1234,1" \
+    "qSearch:memory:1234;2;12" \
+    "s" \
+    "S00" \
+    "T1" \
+    "vCont;c" \
+    "x1234,1" \
+    "X1234,1:00" \
+    "z0,0x1234,0" \
+    "Z0,0x1234,0" \
+    "z1,0x1234,0" \
+    "Z1,0x1234,0" \
+    "z2,0x1234,0" \
+    "Z2,0x1234,0" \
+    "z3,0x1234,0" \
+    "Z3,0x1234,0" \
+    "z4,0x1234,0" \
+    "Z4,0x1234,0" \
+  }
+
+foreach_with_prefix packet $packets {
+    gdb_test "maintenance packet $packet" \
+	"received: \"E01\"" "server returns error"
+}
+
+gdb_test_no_output "set debug remote off"

commit 971b0152e92d381917bb089b3783959c5feb552e

gdb, testsuite: fix regression in gdb.multi/multi-{exit, kill}.exp
Commit d19862435df418a2ec78d078048f54d115c67a66 ("gdbserver:
require_running_or_break for the 'z' and 'vCont' packets") introduced
regressions in gdb.multi/multi-exit.exp and gdb.multi/multi-kill.exp.

If the remote target has multiple inferiors and is resumed with
schedule-multi on, all processes may terminate, making the target have
no threads left.  The target reports termination events to GDB.  GDB
processes the first event and attempts to stop all processes.  For
this, it sends 'vCont;t' packets to the target.  But the patch mentioned
above required the target to be in a running state, which is not true
anymore.  Therefore, target responds with an error.

Fix the regression by reverting the 'require_running_or_return'
enforcement for vCont.

Approved-By: Andrew Burgess <aburgess@redhat.com>

gdbserver/server.cc
@@ -3575,7 +3575,6 @@ handle_v_requests (char *own_buf, int packet_len, int *new_packet_len)
 
       if (startswith (own_buf, "vCont;"))
 	{
-	  require_running_or_return (own_buf);
 	  handle_v_cont (own_buf);
 	  return;
 	}

gdb/testsuite/gdb.server/require-running.exp
@@ -49,7 +49,6 @@ set packets \
     "s" \
     "S00" \
     "T1" \
-    "vCont;c" \
     "x1234,1" \
     "X1234,1:00" \
     "z0,0x1234,0" \