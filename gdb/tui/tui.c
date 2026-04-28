bbf82673430550060bbf31271cd4c81568b26cf5
Andrew Burgess <aburgess@redhat.com>
2026-04-28 11:06:48 +0100

    gdb/tui: prevent TUI activation from a secondary prompt
    The TUI can be activated with key combinations like 'C-x C-a'.  This
    is handled by readline calling the function tui_rl_switch_mode, or
    various other functions which indirectly call that function.
    
    These multi-key combinations can be used even at a secondary prompt,
    e.g. the:
    
      Make breakpoint pending on future shared library load? (y or [n])
    
    The problem with this is that when the TUI activates CLI content
    doesn't carry over into the `cmd` window, so the secondary prompt is
    not visible to the user after the mode switch.  Worse, because the
    content doesn't carry over we clear the readline state, and this
    involves sending a '\n' to readline.  This newline will select the
    default action at the secondary prompt, which might not be what the
    user actually wants.
    
    Now, we could imagine trying to "fix" this so that the CLI content is
    copied over into the `cmd` window, and the secondary prompt is
    represented to the user, so they can then make the choice they want,
    but implementing this fix would be a big job, for very little gain.
    
    I think it is easier to just prevent the user switching to TUI mode
    while at a secondary prompt.
    
    I created a new templated wrapper function tui_rl_keybinding, which is
    then used to wrap every function that is bound to a readline multi-key
    combination.  The wrapper function checks if we are in a secondary
    prompt, and if we are, performs an early return.
    
    For completeness, I've added an assert that we are not in a secondary
    prompt to all of the wrapped functions, this (hopefully) will help
    catch cases where these functions are called directly without going
    through the wrapper.  I also added the same assert to
    tui_rl_command_key and tui_rl_command_mode which are not themselves
    wrapped functions, but are only used when in single key mode, and it
    is not possible to enter single key mode when at a separate prompt,
    see tui_rl_startup_hook (which checks for being at a secondary prompt)
    and tui_rl_next_keymap (which is wrapped).
    
    There's a new helper proc added to lib/gdb.exp, this will be used by
    additional tests later in this series.
    
    The user can still switch to TUI mode at the primary '(gdb)' prompt.

--- gdb/tui/tui.c
+++ gdb/tui/tui.c
@@ -114,6 +114,7 @@ static Keymap tui_readline_standard_keymap;
 static int
 tui_rl_switch_mode (int notused1 = 0, int notused2 = 0)
 {
+  gdb_assert (!gdb_in_secondary_prompt_p (current_ui));
 
   /* Don't let exceptions escape.  We're in the middle of a readline
      callback that isn't prepared for that.  */
@@ -198,6 +199,8 @@ tui_try_activate ()
 static int
 tui_rl_change_windows (int notused1, int notused2)
 {
+  gdb_assert (!gdb_in_secondary_prompt_p (current_ui));
+
   if (tui_try_activate ())
     tui_next_layout ();
 
@@ -209,6 +212,8 @@ tui_rl_change_windows (int notused1, int notused2)
 static int
 tui_rl_delete_other_windows (int notused1, int notused2)
 {
+  gdb_assert (!gdb_in_secondary_prompt_p (current_ui));
+
   if (tui_try_activate ())
     tui_remove_some_windows ();
 
@@ -220,6 +225,8 @@ tui_rl_delete_other_windows (int notused1, int notused2)
 static int
 tui_rl_other_window (int count, int key)
 {
+  gdb_assert (!gdb_in_secondary_prompt_p (current_ui));
+
   if (tui_try_activate ())
     tui_set_win_focus_to (tui_next_win (tui_win_with_focus ()));
 
@@ -231,10 +238,10 @@ tui_rl_other_window (int count, int key)
 static int
 tui_rl_command_key (int count, int key)
 {
-  int i;
+  gdb_assert (!gdb_in_secondary_prompt_p (current_ui));
 
   reinitialize_more_filter ();
-  for (i = 0; tui_commands[i].cmd; i++)
+  for (int i = 0; tui_commands[i].cmd; i++)
     {
       if (tui_commands[i].key == key)
 	{
@@ -262,6 +269,8 @@ tui_rl_command_key (int count, int key)
 static int
 tui_rl_command_mode (int count, int key)
 {
+  gdb_assert (!gdb_in_secondary_prompt_p (current_ui));
+
   tui_set_key_mode (TUI_ONE_COMMAND_MODE);
   return rl_insert (count, key);
 }
@@ -271,6 +280,8 @@ tui_rl_command_mode (int count, int key)
 static int
 tui_rl_next_keymap (int notused1, int notused2)
 {
+  gdb_assert (!gdb_in_secondary_prompt_p (current_ui));
+
   if (!tui_try_activate ())
     return 0;
 
@@ -310,6 +321,20 @@ tui_set_key_mode (enum tui_key_mode mode)
   tui_show_status_content ();
 }
 
+/* Wrapper around function FPTR, used to add common checks before
+   functions that are bound to readline multi-key combinations.  */
+
+template<int (*FPTR) (int, int)>
+int
+tui_rl_keybinding (int count, int key)
+{
+  /* Don't allow TUI changes while we're at an interactive prompt.  */
+  if (gdb_in_secondary_prompt_p (current_ui))
+    return 0;
+
+  return FPTR (count, key);
+}
+
 /* Initialize readline and configure the keymap for the switching
    key shortcut.  */
 void
@@ -324,11 +349,16 @@ tui_ensure_readline_initialized ()
   int i;
   Keymap tui_ctlx_keymap;
 
-  rl_add_defun ("tui-switch-mode", tui_rl_switch_mode, -1);
-  rl_add_defun ("next-keymap", tui_rl_next_keymap, -1);
-  rl_add_defun ("tui-delete-other-windows", tui_rl_delete_other_windows, -1);
-  rl_add_defun ("tui-change-windows", tui_rl_change_windows, -1);
-  rl_add_defun ("tui-other-window", tui_rl_other_window, -1);
+  rl_add_defun ("tui-switch-mode",
+		tui_rl_keybinding<tui_rl_switch_mode>, -1);
+  rl_add_defun ("next-keymap",
+		tui_rl_keybinding<tui_rl_next_keymap>, -1);
+  rl_add_defun ("tui-delete-other-windows",
+		tui_rl_keybinding<tui_rl_delete_other_windows>, -1);
+  rl_add_defun ("tui-change-windows",
+		tui_rl_keybinding<tui_rl_change_windows>, -1);
+  rl_add_defun ("tui-other-window",
+		tui_rl_keybinding<tui_rl_other_window>, -1);
 
   tui_keymap = rl_make_bare_keymap ();
 
@@ -361,21 +391,21 @@ tui_ensure_readline_initialized ()
       rl_bind_key_in_map (i, tui_rl_command_mode, tui_keymap);
     }
 
-  rl_bind_key_in_map ('a', tui_rl_switch_mode, emacs_ctlx_keymap);
-  rl_bind_key_in_map ('a', tui_rl_switch_mode, tui_ctlx_keymap);
-  rl_bind_key_in_map ('A', tui_rl_switch_mode, emacs_ctlx_keymap);
-  rl_bind_key_in_map ('A', tui_rl_switch_mode, tui_ctlx_keymap);
-  rl_bind_key_in_map (c_ctrl ('A'), tui_rl_switch_mode, emacs_ctlx_keymap);
-  rl_bind_key_in_map (c_ctrl ('A'), tui_rl_switch_mode, tui_ctlx_keymap);
-  rl_bind_key_in_map ('1', tui_rl_delete_other_windows, emacs_ctlx_keymap);
-  rl_bind_key_in_map ('1', tui_rl_delete_other_windows, tui_ctlx_keymap);
-  rl_bind_key_in_map ('2', tui_rl_change_windows, emacs_ctlx_keymap);
-  rl_bind_key_in_map ('2', tui_rl_change_windows, tui_ctlx_keymap);
-  rl_bind_key_in_map ('o', tui_rl_other_window, emacs_ctlx_keymap);
-  rl_bind_key_in_map ('o', tui_rl_other_window, tui_ctlx_keymap);
-  rl_bind_key_in_map ('q', tui_rl_next_keymap, tui_keymap);
-  rl_bind_key_in_map ('s', tui_rl_next_keymap, emacs_ctlx_keymap);
-  rl_bind_key_in_map ('s', tui_rl_next_keymap, tui_ctlx_keymap);
+  rl_bind_key_in_map ('a', tui_rl_keybinding<tui_rl_switch_mode>, emacs_ctlx_keymap);
+  rl_bind_key_in_map ('a', tui_rl_keybinding<tui_rl_switch_mode>, tui_ctlx_keymap);
+  rl_bind_key_in_map ('A', tui_rl_keybinding<tui_rl_switch_mode>, emacs_ctlx_keymap);
+  rl_bind_key_in_map ('A', tui_rl_keybinding<tui_rl_switch_mode>, tui_ctlx_keymap);
+  rl_bind_key_in_map (c_ctrl ('A'), tui_rl_keybinding<tui_rl_switch_mode>, emacs_ctlx_keymap);
+  rl_bind_key_in_map (c_ctrl ('A'), tui_rl_keybinding<tui_rl_switch_mode>, tui_ctlx_keymap);
+  rl_bind_key_in_map ('1', tui_rl_keybinding<tui_rl_delete_other_windows>, emacs_ctlx_keymap);
+  rl_bind_key_in_map ('1', tui_rl_keybinding<tui_rl_delete_other_windows>, tui_ctlx_keymap);
+  rl_bind_key_in_map ('2', tui_rl_keybinding<tui_rl_change_windows>, emacs_ctlx_keymap);
+  rl_bind_key_in_map ('2', tui_rl_keybinding<tui_rl_change_windows>, tui_ctlx_keymap);
+  rl_bind_key_in_map ('o', tui_rl_keybinding<tui_rl_other_window>, emacs_ctlx_keymap);
+  rl_bind_key_in_map ('o', tui_rl_keybinding<tui_rl_other_window>, tui_ctlx_keymap);
+  rl_bind_key_in_map ('q', tui_rl_keybinding<tui_rl_next_keymap>, tui_keymap);
+  rl_bind_key_in_map ('s', tui_rl_keybinding<tui_rl_next_keymap>, emacs_ctlx_keymap);
+  rl_bind_key_in_map ('s', tui_rl_keybinding<tui_rl_next_keymap>, tui_ctlx_keymap);
 
   /* Initialize readline after the above.  */
   rl_initialize ();
