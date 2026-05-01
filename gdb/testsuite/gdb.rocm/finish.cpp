59aa25ef9e36d9534c1e123d5fce4e3df451278e
Lancelot SIX <lancelot.six@amd.com>
2026-05-01 19:10:08 +0100

    gdb/testsuite/gdb.rocm/finish: Test small mixed struct
    This patch extends the gdb.rocm/finish.exp testcase to cover small
    (up to 64 bytes) struct containing some sub 32 bits elements (short in
    this case) and a float element.  This test is here to extend our
    coverage of the existing ABI.
    
    Suggested-by: Qadeer, Hafiz Abid <HafizAbid.Qadeer@amd.com>
    Change-Id: I2a56ddd7712e58d2f4fa5da21182bd4e76b1873e

--- gdb/testsuite/gdb.rocm/finish.cpp
+++ gdb/testsuite/gdb.rocm/finish.cpp
@@ -82,6 +82,12 @@ struct NotPackedWithBitField
   char c6 : 8;
 };
 
+struct MixedSmallStruct
+{
+  float a;
+  short b[2];
+};
+
 struct OnlyStatic
 {
   __device__ static int something;
@@ -211,6 +217,17 @@ returnEmptyWithStatic ()
   return {};
 }
 
+__device__ static __attribute__((noinline)) MixedSmallStruct
+returnMixedSmallSruct ()
+{
+  MixedSmallStruct var;
+  var.b[0] = 4;
+  var.b[1] = 7;
+  var.a = 3.14;
+
+  return var;
+}
+
 __device__ int someGlobal = 42;
 
 __global__ void
@@ -218,6 +235,8 @@ kernel ()
 {
   returnEmpty ();
   returnEmptyWithStatic ();
+
+  returnMixedSmallSruct ();
 
   returnSized<1> ();
   returnSized<2> ();
