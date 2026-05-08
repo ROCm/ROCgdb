0166d3cedefcd8f60417427f7b72f46e74a35290
Nick Clifton <nickc@redhat.com>
2026-05-08 11:10:05 +0100

    Objdump: Add "-M annotate" option to the AArch64 disassembler.

---
+++ binutils/testsuite/binutils-all/aarch64/objdumpM2.c
@@ -0,0 +1,5 @@
+int func1 (int arg) { return arg * 2; }
+int func2 (int arg) { return arg * 3; }
+int func3 (int arg) { return arg * 4; }
+int func4 (int arg) { return arg * 5; }
+int func5 (int arg) { return arg * 6; }
