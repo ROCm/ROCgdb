22143ca474bb7154a5c9b4aa20a3aba256c0137a
Pedro Alves <pedro@palves.net>
2026-05-20 11:22:56 +0100

    Adjust gdb.python/py-events.exp for Cygwin/MinGW, no fork
    The gdb.python/py-events.exp testcase currently uses fork, and relies
    on "set detach-on-fork off".
    
    Cygwin does support the fork syscall, but GDB can't follow forks
    there, so "detach-on-fork off" has no effect.  And also, there is no
    fork on native Windows, which makes the testcase unusable on MinGW
    currently.
    
    I don't see any reason the testcase needs to use fork or multiple
    inferiors.  We can replace what those parts were testing by more
    focused tests:
    
     - The clear_objfiles event was tested via following a fork.  Instead,
       test a more directed "file" command.
    
     - Two-inferior quit was being used to test that gdb.ExitedEvent has
       no "exit_code".  Quitting while an inferior is being debugged makes
       GDB kill the inferior.  What's really being tested is the kill
       path, so write an explicit (single-inferior) kill test.
    
    Tested on x86_64-unknown-linux-gnu.
    
    Change-Id: I21ee8af7b52653c6fdff9b4c1596cdde3cfe751a

--- gdb/testsuite/gdb.python/py-events.c
+++ gdb/testsuite/gdb.python/py-events.c
@@ -15,12 +15,9 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see  <http://www.gnu.org/licenses/>.  */
 
-#include <unistd.h>
-
 extern void do_nothing (void);
 
 int second(){
-  fork() ;
   return 12;
 }
 
