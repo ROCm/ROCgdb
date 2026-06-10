bminor/binutils-gdb

commit 45a7f5a29de78755dd0d5e8f7beaf2bd07736d20

dlltool leading_underscore
This patch tidies dlltool code dealing with adding a leading
underscore to generated symbol names.  There should be no functional
change here, but there could be if we ever have a bfd target with
symbol_leading_char something other than '_' or 0.

	* dlltool.c (leading_underscore): Change from an int to a
        char*.  Update all uses.  If neither --leading-underscore or
        --no=leading-underscore is given, set leading_underscore to a
        string with first char returned by bfd_get_target_info as the
        target's symbol underscoring.

binutils/dlltool.c
@@ -402,11 +402,7 @@ static int dll_name_set_by_exp_name;
 static int add_indirect = 0;
 static int add_underscore = 0;
 static int add_stdcall_underscore = 0;
-/* This variable can hold three different values. The value
-   -1 (default) means that default underscoring should be used,
-   zero means that no underscoring should be done, and one
-   indicates that underscoring should be done.  */
-static int leading_underscore = -1;
+static char *leading_underscore = NULL;
 static int dontdeltemps = 0;
 
 /* TRUE if we should export all symbols.  Otherwise, we only export
@@ -909,31 +905,13 @@ rvabefore (int mach)
 static const char *
 asm_prefix (int mach, const char *name)
 {
-  switch (mach)
-    {
-    case MARM:
-    case MTHUMB:
-    case MARM_INTERWORK:
-    case MMCORE_BE:
-    case MMCORE_LE:
-    case MMCORE_ELF:
-    case MMCORE_ELF_LE:
-    case MARM_WINCE:
-    case MAARCH64:
-      break;
-    case M386:
-    case MX86:
-      /* Symbol names starting with ? do not have a leading underscore. */
-      if ((name && *name == '?') || leading_underscore == 0)
-	break;
-      else
-	return "_";
-    default:
-      /* xgettext:c-format */
-      fatal (_("Internal error: Unknown machine type: %d"), mach);
-      break;
-    }
-  return "";
+  /* ??? --leading_underscore is only supported on x86.  */
+  if (!(mach == M386 || mach == MX86))
+    return "";
+  /* Symbol names starting with ? do not have a leading underscore.  */
+  if (name && *name == '?')
+    return "";
+  return leading_underscore;
 }
 
 #define ASM_BYTE		mtable[machine].how_byte
@@ -1500,7 +1478,7 @@ add_excludes (const char *new_excludes)
       if (*exclude_string == '@')
 	sprintf (new_exclude->string, "%s", exclude_string);
       else
-	sprintf (new_exclude->string, "%s%s", !leading_underscore ? "" : "_",
+	sprintf (new_exclude->string, "%s%s", leading_underscore,
 		 exclude_string);
       new_exclude->next = excludes;
       excludes = new_exclude;
@@ -2150,11 +2128,10 @@ gen_exp_file (void)
 	    if (create_compat_implib)
 	      fprintf (f, "\t%s\t__imp_%s\n", ASM_GLOBAL, exp->name);
 	    fprintf (f, "\t%s\t_imp_%s%s\n", ASM_GLOBAL,
-		     !leading_underscore ? "" : "_", exp->name);
+		     leading_underscore, exp->name);
 	    if (create_compat_implib)
 	      fprintf (f, "__imp_%s:\n", exp->name);
-	    fprintf (f, "_imp_%s%s:\n",
-		     !leading_underscore ? "" : "_", exp->name);
+	    fprintf (f, "_imp_%s%s:\n", leading_underscore, exp->name);
 	    fprintf (f, "\t%s\t%s\n", ASM_LONG, exp->name);
 	  }
     }
@@ -3874,10 +3851,10 @@ main (int ac, char **av)
 	  add_stdcall_underscore = 1;
 	  break;
 	case OPTION_NO_LEADING_UNDERSCORE:
-	  leading_underscore = 0;
+	  leading_underscore = "";
 	  break;
 	case OPTION_LEADING_UNDERSCORE:
-	  leading_underscore = 1;
+	  leading_underscore = "_";
 	  break;
 	case OPTION_IDENTIFY_STRICT:
 	  identify_strict = 1;
@@ -4008,12 +3985,18 @@ main (int ac, char **av)
 		    || strcmp (mname, "arm64") == 0);
 
   /* Check the default underscore */
-  int u = leading_underscore; /* Underscoring mode. -1 for use default.  */
-  if (u == -1)
-    bfd_get_target_info (mtable[machine].how_bfd_target, NULL,
-			 NULL, &u, NULL);
-  if (u != -1)
-    leading_underscore = u != 0;
+  if (leading_underscore == NULL)
+    {
+      int u;
+      static char underscore[2];
+      bfd_get_target_info (mtable[machine].how_bfd_target, NULL,
+			   NULL, &u, NULL);
+      if (u == -1)
+	u = 0;
+      underscore[0] = u;
+      underscore[1] = 0;
+      leading_underscore = underscore;
+    }
 
   if (!dll_name && exp_name)
     {

ROCm/ROCgdb

commit 500390720267e2f009f0354e5051fa2e176b143d

PR 34204 dlltool SEGVs with --exclude-symbols
This fixes a segfault introduced by commit 45a7f5a29de7 which didn't
take into account that there was a use of leading_underscore in option
processing.

	PR 34204
	* dlltool.c (struct string_list): Make string a flexible array
	member.
	(add_excludes): Adjust to suit.  Extract code adding underscore
	and informing..
	(underscore_excludes): ..to here.  New function.
	(main): Call underscore_excludes.

binutils/dlltool.c
@@ -789,7 +789,7 @@ export_type;
 struct string_list
 {
   struct string_list *next;
-  char *string;
+  char string[];
 };
 
 static struct string_list *excludes;
@@ -1460,21 +1460,35 @@ add_excludes (const char *new_excludes)
   exclude_string = strtok (local_copy, ",:");
   for (; exclude_string; exclude_string = strtok (NULL, ",:"))
     {
-      struct string_list *new_exclude = xmalloc (sizeof (*new_exclude));
-      /* Don't add a leading underscore for fastcall symbols.  */
-      if (*exclude_string == '@')
-	new_exclude->string = xstrdup (exclude_string);
-      else
-	new_exclude->string = xasprintf ("%s%s", leading_underscore,
-					 exclude_string);
+      size_t len = strlen (exclude_string);
+      /* Allocate extra byte for possible underscore.  */
+      struct string_list *new_exclude = xmalloc (sizeof (*new_exclude)
+						 + len + 2);
+      memcpy (new_exclude->string, exclude_string, len + 1);
       new_exclude->next = excludes;
       excludes = new_exclude;
-
-      /* xgettext:c-format */
-      inform (_("Excluding symbol: %s"), exclude_string);
     }
 
   free (local_copy);
+}
+
+/* Prefix symbols on the excludes list with an underscore.  */
+
+static void
+underscore_excludes (void)
+{
+  for (struct string_list *ex = excludes; ex; ex = ex->next)
+    {
+      /* Don't add a leading underscore for fastcall symbols.  */
+      if (*ex->string != '@' && *leading_underscore)
+	{
+	  size_t len = strlen (ex->string);
+	  memmove (ex->string + 1, ex->string, len + 1);
+	  *ex->string = *leading_underscore;
+	}
+      /* xgettext:c-format */
+      inform (_("Excluding symbol: %s"), ex->string);
+    }
 }
 
 /* See if STRING is on the list of symbols to exclude.  */
@@ -4064,6 +4078,8 @@ main (int ac, char **av)
 
   if (do_default_excludes)
     set_default_excludes ();
+
+  underscore_excludes ();
 
   if (def_file)
     process_def_file (def_file);