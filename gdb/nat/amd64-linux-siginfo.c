ROCm/ROCgdb

commit 6c85ef111b0430493792cde3b0fd572f1c78e732

[gdb] Use using instead of typedef
After commit 1eed06ae51d ("[gdbsupport] Use using instead of typedef in
next_iterator") I wondered if I could do something similar using sed.

Result of:
...
$ find gdb* -type f -name "*.[ch]" -o -name "*.cc" \
    | egrep -v /testsuite/ \
    | xargs sed -i \
	    's/^\([ \t]*\)typedef \([a-zA-Z_0-9:<>,.() ]*\) \([\*&][\*&]*\)\([a-zA-Z_0-9]*\);/\1using \4 = \2 \3;/'

$ find gdb* -type f -name "*.[ch]" -o -name "*.cc" \
    | egrep -v /testsuite/ \
    | xargs sed -i \
	    's/^\([ \t]*\)typedef \([a-zA-Z_0-9:<>,.() ]*\) \([a-zA-Z_0-9]*\);/\1using \3 = \2;/'
...

Tested on x86_64-linux.

gdb/nat/amd64-linux-siginfo.c
@@ -35,14 +35,14 @@
 
 #ifndef __ILP32__
 
-typedef int nat_int_t;
-typedef unsigned long nat_uptr_t;
+using nat_int_t = int;
+using nat_uptr_t = unsigned long;
 
-typedef int nat_time_t;
-typedef int nat_timer_t;
+using nat_time_t = int;
+using nat_timer_t = int;
 
 /* For native 64-bit, clock_t in _sigchld is 64-bit.  */
-typedef long nat_clock_t;
+using nat_clock_t = long;
 
 union nat_sigval_t
 {
@@ -119,12 +119,12 @@ struct nat_siginfo_t
    compatible with the siginfo type exported by the 32-bit userspace
    support.  */
 
-typedef int compat_int_t;
-typedef unsigned int compat_uptr_t;
+using compat_int_t = int;
+using compat_uptr_t = unsigned int;
 
-typedef int compat_time_t;
-typedef int compat_timer_t;
-typedef int compat_clock_t;
+using compat_time_t = int;
+using compat_timer_t = int;
+using compat_clock_t = int;
 
 struct compat_timeval
 {
@@ -203,7 +203,7 @@ struct compat_siginfo_t
 };
 
 /* For x32, clock_t in _sigchld is 64bit aligned at 4 bytes.  */
-typedef long __attribute__ ((__aligned__ (4))) compat_x32_clock_t;
+using compat_x32_clock_t = long __attribute__ ((__aligned__ (4)));
 
 struct __attribute__ ((__aligned__ (8))) compat_x32_siginfo_t
 {
@@ -299,9 +299,9 @@ struct __attribute__ ((__aligned__ (8))) compat_x32_siginfo_t
    PTRACE_GETSIGINFO.  If gdb is built as a x32 program, we get a x32
    siginfo.  */
 #ifdef __ILP32__
-typedef compat_x32_siginfo_t ptrace_siginfo_t;
+using ptrace_siginfo_t = compat_x32_siginfo_t;
 #else
-typedef nat_siginfo_t ptrace_siginfo_t;
+using ptrace_siginfo_t = nat_siginfo_t;
 #endif
 
 /*  Convert the system provided siginfo into compatible siginfo.  */

commit 8b752803cf57408cfb7a50bc77a3cbc658f7ad23

[gdb/build, clang] Fix ignored aligned attribute
PR build/34279 reports a build failure with clang.

This is a regression since commit 6c85ef111b0 ("[gdb] Use using instead of
typedef"), which did:
...
-typedef long __attribute__ ((__aligned__ (4))) compat_x32_clock_t;
+using compat_x32_clock_t = long __attribute__ ((__aligned__ (4)));
...

The problem is that clang ignores the attribute.

Fix this by reverting this part of the commit.

Bug: https://sourceware.org/bugzilla/show_bug.cgi?id=34279

gdb/nat/amd64-linux-siginfo.c
@@ -203,7 +203,7 @@ struct compat_siginfo_t
 };
 
 /* For x32, clock_t in _sigchld is 64bit aligned at 4 bytes.  */
-using compat_x32_clock_t = long __attribute__ ((__aligned__ (4)));
+typedef long __attribute__ ((__aligned__ (4))) compat_x32_clock_t;
 
 struct __attribute__ ((__aligned__ (8))) compat_x32_siginfo_t
 {