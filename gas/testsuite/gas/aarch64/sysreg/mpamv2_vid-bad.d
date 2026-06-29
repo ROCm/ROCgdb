fbb2ff78536f848b2df6b8d430b618080745c8f6
Richard Earnshaw <rearnsha@arm.com>
2026-06-29 16:36:22 +0100

    Revert "aarch64: Add FEAT_MPAMv2_VID system registers"
    This reverts commit 5fc6b09c078532fa4206a350e34d82488b80a5fb.

--- gas/testsuite/gas/aarch64/sysreg/mpamv2_vid-bad.d
+++
@@ -1,3 +0,0 @@
-#as: -march=armv9.4-a -menable-sysreg-checking -I$srcdir/$subdir
-#source: mpamv2_vid.s
-#error_output: mpamv2_vid-bad.l
--- gas/testsuite/gas/aarch64/sysreg/mpamv2_vid.s
+++
@@ -1,5 +0,0 @@
-.include "sysreg-test-utils.inc"
-
-	rw_sys_reg mpamvidcr_el2
-	rw_sys_reg mpamvidsr_el2
-	rw_sys_reg mpamvidsr_el3
