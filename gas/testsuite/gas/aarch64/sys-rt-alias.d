1973788235f55494e51bfb05d61c4b00983f7ee2
Richard Earnshaw <rearnsha@arm.com>
2026-06-29 17:03:49 +0100

    aarch64: remove MLBI from sys-rt-alias test.

--- gas/testsuite/gas/aarch64/sys-rt-alias.d
+++ gas/testsuite/gas/aarch64/sys-rt-alias.d
@@ -9,8 +9,6 @@ Disassembly of section \.text:
 .*:	d50c8780 	sys	#4, C8, C7, #4, x0
 .*:	d50ca79f 	plbi	alle1
 .*:	d50ca780 	sys	#4, C10, C7, #4, x0
-.*:	d50c709f 	mlbi	alle1
-.*:	d50c7080 	sys	#4, C7, C0, #4, x0
 .*:	d508751f 	ic	iallu
 .*:	d5087500 	sys	#0, C7, C5, #0, x0
 .*:	d50887bf 	tlbi	vale1, xzr
