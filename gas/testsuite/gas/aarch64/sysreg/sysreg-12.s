ffca19ab8e9cf5f57a6ea81a108d9113de9e8336
Alice Carlotti <alice.carlotti@arm.com>
2026-07-10 15:51:50 +0100

    aarch64: Update FEAT_TPS and FEAT_TPSP system registers
    The specification has been changed to use individual tpcr* registers,
    instead of the original tpmin*/tpmax* pairs.

--- gas/testsuite/gas/aarch64/sysreg/sysreg-12.s
+++ gas/testsuite/gas/aarch64/sysreg/sysreg-12.s
@@ -1,18 +1,10 @@
 .include "sysreg-test-utils.inc"
 
-	rw_sys_reg tpmax0_el0
-	rw_sys_reg tpmax0_el1
-	rw_sys_reg tpmax0_el12
-	rw_sys_reg tpmax0_el2
-	rw_sys_reg tpmax1_el0
-	rw_sys_reg tpmax1_el1
-	rw_sys_reg tpmax1_el12
-	rw_sys_reg tpmax1_el2
-	rw_sys_reg tpmin0_el0
-	rw_sys_reg tpmin0_el1
-	rw_sys_reg tpmin0_el12
-	rw_sys_reg tpmin0_el2
-	rw_sys_reg tpmin1_el0
-	rw_sys_reg tpmin1_el1
-	rw_sys_reg tpmin1_el12
-	rw_sys_reg tpmin1_el2
+	rw_sys_reg tpcr0_el0
+	rw_sys_reg tpcr0_el1
+	rw_sys_reg tpcr0_el12
+	rw_sys_reg tpcr0_el2
+	rw_sys_reg tpcr1_el0
+	rw_sys_reg tpcr1_el1
+	rw_sys_reg tpcr1_el12
+	rw_sys_reg tpcr1_el2
