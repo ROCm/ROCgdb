ROCm/ROCgdb

commit e189bfd9b492a0361a37858b9a4709a52a92c803

use linker created stub bfd for dynobj
A linker created bfd is a good choice for somewhere to attach linker
created dynamic sections.  It is always the correct format and doesn't
have anything other than linker generated info to potentially confuse
ld.  It is always the first bfd too, if ordering matters.

For those targets that create a stub bfd, this patch makes use of that
bfd for the linker dynobj.  I've done that by allowing the stub bfd
in _bfd_elf_link_dynobj, and using that function wherever dynobj is
assigned in ELF targets that currently support dynamic linking.

For targets that don't create a stub bfd, _bfd_elf_link_dynobj has
fewer conditions on the bfd chosen for dynobj than those for running
check relocs, so _bfd_elf_link_dynobj return in check_relocs is always
non-NULL.  Despite that I thought it good defensive programming
practice to check the return value anyway.

One testcase needs updating for .dynamic being placed earlier if left
as an orphan.  I chose to provide the missing sections in the test
linker script rather than changing expected output.

bfd/
	* elf-bfd.h (struct elf_link_hash_table <dynobj>): Update comment.
	(_bfd_elf_link_dynobj_internal): Declare.
	(_bfd_elf_link_dynobj): Define.
	* elflink.c (_bfd_elf_link_dynobj_internal): Rename from
	_bfd_elf_link_dynobj.  Delete "abfd" param.  Add "report"
	param.  Don't exclude any object satisfying compatible_format
	from being used as dynobj.  Do complain if "report" and no
	suitable object found.
	* elf-m10300.c (mn10300_elf_check_relocs): Replace assignment
	from abfd to dynobj with call to _bfd_elf_link_dynobj.
	* elf32-arc.c (elf_arc_check_relocs): Likewise.
	* elf32-arm.c (elf32_arm_check_relocs): Likewise.
	* elf32-bfin.c (bfin_check_relocs): Likewise.
	(bfinfdpic_check_relocs): Likewise.
	* elf32-cr16.c (cr16_elf_check_relocs): Likewise.
	* elf32-cris.c (cris_elf_check_relocs): Likewise.
	* elf32-csky.c (csky_elf_check_relocs): Likewise.

bfd/elflink.c
@@ -302,24 +302,20 @@ compatible_format (struct bfd_link_info *info, bfd *ibfd)
 
 /* Find a suitable object for attaching dynamic sections.  */
 
-static bfd *
-_bfd_elf_link_dynobj (struct bfd_link_info *info)
+bfd *
+_bfd_elf_link_dynobj_internal (struct bfd_link_info *info, bool report)
 {
   struct elf_link_hash_table *htab = elf_hash_table (info);
 
   if (htab->dynobj == NULL)
     {
       /* We need to find an input file of the same format as the
-	 output to hold linker created sections.  For now, exclude
-	 any stub bfd.  */
-      bfd *ibfd = info->input_bfds;
-      if ((ibfd->flags
-	   & (BFD_PLUGIN | BFD_LINKER_CREATED)) == BFD_LINKER_CREATED)
-	ibfd = ibfd->link.next;
-      for (; ibfd; ibfd = ibfd->link.next)
+	 output to hold linker created sections.  */
+      bfd *ibfd;
+      for (ibfd = info->input_bfds; ibfd; ibfd = ibfd->link.next)
 	if (compatible_format (info, ibfd))
 	  break;
-      if (ibfd == NULL)
+      if (ibfd == NULL && report)
 	_bfd_error_handler (_("no %s input object found"),
 			    bfd_get_target (info->output_bfd));
       htab->dynobj = ibfd;

bfd/elf-bfd.h
@@ -664,9 +664,7 @@ struct elf_link_hash_table
   /* TRUE if there are base symbols.  */
   bool has_base_symbols;
 
-  /* The BFD used to hold special sections created by the linker.
-     This will be the first BFD found which requires these sections to
-     be created.  */
+  /* The BFD used to hold special sections created by the linker.  */
   bfd *dynobj;
 
   /* The value to use when initialising got.refcount/offset and
@@ -2730,6 +2728,12 @@ extern unsigned int _bfd_elf_default_action_discarded
   (struct bfd_section *) ATTRIBUTE_HIDDEN;
 extern struct bfd_section *bfd_elf_tls_setup
   (struct bfd_link_info *);
+
+extern bfd *_bfd_elf_link_dynobj_internal
+  (struct bfd_link_info *, bool) ATTRIBUTE_HIDDEN;
+static inline bfd *
+_bfd_elf_link_dynobj (struct bfd_link_info *info)
+{ return _bfd_elf_link_dynobj_internal (info, true); }
 
 extern bool bfd_elf_link_create_dynamic_sections
   (struct bfd_link_info *);

bfd/elf-m10300.c
@@ -1115,7 +1115,9 @@ mn10300_elf_check_relocs (bfd *abfd,
 	    case R_MN10300_TLS_LD:
 	    case R_MN10300_TLS_GOTIE:
 	    case R_MN10300_TLS_IE:
-	      elf_hash_table (info)->dynobj = dynobj = abfd;
+	      dynobj = _bfd_elf_link_dynobj (info);
+	      if (dynobj == NULL)
+		goto fail;
 	      if (! _bfd_mn10300_elf_create_got_section (dynobj, info))
 		goto fail;
 	      break;

bfd/elf32-arc.c
@@ -1987,10 +1987,10 @@ elf_arc_check_relocs (bfd *			 abfd,
   if (bfd_link_relocatable (info))
     return true;
 
-  if (htab->dynobj == NULL)
-    htab->dynobj = abfd;
+  dynobj = _bfd_elf_link_dynobj (info);
+  if (dynobj == NULL)
+    return false;
 
-  dynobj = (elf_hash_table (info))->dynobj;
   symtab_hdr = &elf_symtab_hdr (abfd);
   sym_hashes = elf_sym_hashes (abfd);
 

bfd/elf32-arm.c
@@ -15216,13 +15216,12 @@ elf32_arm_check_relocs (bfd *abfd, struct bfd_link_info *info,
     return false;
 
   sreloc = NULL;
-
-  if (htab->root.dynobj == NULL)
-    htab->root.dynobj = abfd;
-  if (!create_ifunc_sections (info))
+  dynobj = _bfd_elf_link_dynobj (info);
+  if (dynobj == NULL)
     return false;
 
-  dynobj = htab->root.dynobj;
+  if (!create_ifunc_sections (info))
+    return false;
 
   symtab_hdr = & elf_symtab_hdr (abfd);
   sym_hashes = elf_sym_hashes (abfd);

bfd/elf32-bfin.c
@@ -1226,7 +1226,11 @@ bfin_check_relocs (bfd * abfd,
 	  /* Fall through.  */
 
 	  if (dynobj == NULL)
-	    elf_hash_table (info)->dynobj = dynobj = abfd;
+	    {
+	      dynobj = _bfd_elf_link_dynobj (info);
+	      if (dynobj == NULL)
+		return false;
+	    }
 
 	  if (!_bfd_elf_create_got_section (dynobj, info))
 	    return false;
@@ -4559,7 +4563,11 @@ bfinfdpic_check_relocs (bfd *abfd, struct bfd_link_info *info,
 	  if (IS_FDPIC (abfd))
 	    {
 	      if (dynobj == NULL)
-		elf_hash_table (info)->dynobj = dynobj = abfd;
+		{
+		  dynobj = _bfd_elf_link_dynobj (info);
+		  if (dynobj == NULL)
+		    return false;
+		}
 	      if (!_bfin_create_got_section (dynobj, info))
 		return false;
 	    }

bfd/elf32-cr16.c
@@ -744,7 +744,9 @@ cr16_elf_check_relocs (bfd *abfd, struct bfd_link_info *info, asection *sec,
 	    {
 	    case R_CR16_GOT_REGREL20:
 	    case R_CR16_GOTC_REGREL20:
-	      elf_hash_table (info)->dynobj = dynobj = abfd;
+	      dynobj = _bfd_elf_link_dynobj (info);
+	      if (dynobj == NULL)
+		goto fail;
 	      if (! _bfd_cr16_elf_create_got_section (dynobj, info))
 		goto fail;
 	      break;

bfd/elf32-cris.c
@@ -3036,7 +3036,11 @@ cris_elf_check_relocs (bfd *abfd,
 	case R_CRIS_16_GOTPLT:
 	case R_CRIS_32_GOTPLT:
 	  if (dynobj == NULL)
-	    elf_hash_table (info)->dynobj = dynobj = abfd;
+	    {
+	      dynobj = _bfd_elf_link_dynobj (info);
+	      if (dynobj == NULL)
+		return false;
+	    }
 
 	  if (sgot == NULL)
 	    {

commit ffd2a7258c46d74d960502b3b3f77aaa44b8118b

Delay writing of stub bfd sections
AARCH64 continues an ARM tradition of updating stubs late.  See
coff_arm_link_output_has_begun.  Commit e189bfd9b492 broke AARCH64,
because adding dynamic sections to the stub bfd resulted in that
entire bfd being written out fairly early.  Prior to e189bfd9b492
dynamic sections were usually added to crt1.o, the first object being
linked.  If crt1.o happened to need an erratum_843419 stub I think
we'd see the same sort of breakage exposed by e189bfd9b492, with later
object files' erratum_843419 stubs not being updated (resulting in
an all-zero insn in the stub).  You would likely hit the same problem
if user linker scripts divided up code sections for some reason.

There likely is no reason to clear sub->output_has_begun here, but
I'll leave removing that to a followup patch.

	* elflink.c (_bfd_elf_final_link): Write any linker created
	bfd last.

bfd/elflink.c
@@ -13193,7 +13193,8 @@ _bfd_elf_final_link (bfd *obfd, struct bfd_link_info *info)
 	    {
 	      if (! sub->output_has_begun)
 		{
-		  if (! elf_link_input_bfd (&flinfo, sub))
+		  if ((sub->flags & BFD_LINKER_CREATED) == 0
+		      && !elf_link_input_bfd (&flinfo, sub))
 		    goto error_return;
 		  sub->output_has_begun = true;
 		}
@@ -13244,6 +13245,17 @@ _bfd_elf_final_link (bfd *obfd, struct bfd_link_info *info)
 	    }
 	}
     }
+  /* Writing of linker created BFDs is left until last, because the
+     aarch64 backend wants to copy insns from a relocated section to
+     a stub section.  See erratum_843419 code.  */
+  for (sub = info->input_bfds; sub != NULL; sub = sub->link.next)
+    if (sub->output_has_begun && (sub->flags & BFD_LINKER_CREATED) != 0)
+      {
+	sub->output_has_begun = false;
+	if (!elf_link_input_bfd (&flinfo, sub))
+	  goto error_return;
+	sub->output_has_begun = true;
+      }
 
   /* Free symbol buffer if needed.  */
   if (!info->reduce_memory_overheads)