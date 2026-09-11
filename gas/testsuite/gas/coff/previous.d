636a636c473a55d3420ac4bffd1dd4c223b96408
Johannes Khoshnazar-Thoma <johannes@johannesthoma.com>
2026-09-11 13:44:56 +0200

    gas/coff: support for .previous and .{push,pop}section pseudo ops
    Add support for the .previous, .pushsection and .popsection pseudo-ops for
    COFF targets. The semantics are the same as their ELF counterparts.
    
    This patch is one building block for compiling the Linux kernel
    as a Windows/ReactOS driver.
    
    Signed-off-by: Johannes Khoshnazar-Thoma <johannes@johannesthoma.com>

---
+++ gas/testsuite/gas/coff/previous.d
@@ -0,0 +1,9 @@
+#objdump: -s
+#name: .previous support
+
+.*:     file format .*
+
+Contents of section a:
+ 0000.*01[0 ]*03.*
+Contents of section b:
+ 0000.*02[0 ]*04.*
---
+++ gas/testsuite/gas/coff/pushpop.s
@@ -0,0 +1,6 @@
+	.section a
+	.byte 1
+	.pushsection b
+	.byte 2
+	.popsection
+	.byte 3
